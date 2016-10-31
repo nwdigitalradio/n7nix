268a3ea39a4b21684622c0ce13e0f853bd94305c
n7nix <basil@pacabunga.com>
2016-10-31 13:51:55 -0700

    Add prompts for call sign & ssid, make useable.

--- ax25/install.sh
+++ ax25/install.sh
@@ -1,15 +1,51 @@
 #!/bin/bash
 #
+# Configure axports & ax25d.conf files
+#
 # Uncomment this statement for debug echos
 DEBUG=1
 
 CALLSIGN="N0NE"
-SSID="4"
 AX25PORT="udr0"
+SSID="15"
+AX25DSSID="0"
+AX25_CFGDIR="/usr/local/etc/ax25"
 
 function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
 
-if [ ! -f "/etc/ax25/axports" ] ; then
+# ===== function prompt_read
+
+function prompt_read() {
+echo "Enter call sign, followed by [enter]:"
+read CALLSIGN
+
+sizecallstr=${#CALLSIGN}
+
+if (( sizecallstr > 6 )) || ((sizecallstr < 3 )) ; then
+   echo "Invalid call sign: $CALLSIGN, length = $sizecallstr"
+   exit 1
+fi
+
+CALLSIGN=$(echo "$CALLSIGN" | tr '[a-z]' '[A-Z]')
+dbgecho "Using CALL SIGN: $CALLSIGN"
+
+echo "Enter ssid, followed by [enter]:"
+read SSID
+
+sizessidstr=${#SSID}
+
+if (( sizessidstr > 2 )) || ((sizessidstr < 0 )) ; then
+   echo "Invalid ssid: $SSID, length = $sizessidstr"
+   exit 1
+fi
+
+dbgecho "Using SSID: $SSID"
+
+}
+
+# ===== main
+
+if [ ! -f "/etc/ax25/axports" ] && [ ! -f "$AX25_CFGDIR/axports" ] ; then
    echo "Need to install libax25, tools & apps"
    exit 1
 fi
@@ -20,14 +56,49 @@ if [[ $EUID != 0 ]] ; then
    exit 1
 fi
 
-grep -i "udr0" /etc/ax25/axports
+
+grep -i "udr0" $AX25_CFGDIR/axports
 if [ $? -eq 1 ] ; then
    echo "No ax25 ports defined"
+   mv $AX25_CFGDIR/axports $AX25_CFGDIR/axports-dist
+   echo "Original ax25ports saved as axports-dist"
+
+   prompt_read
 {
-echo "$AX25PORT            $CALLSIGN-$SSID         9600    255     2       APRS"
-} >> /etc/ax25/axports
+echo "# $AX25_CFGDIR/axports"
+echo "#"
+echo "# The format of this file is:"
+echo "#portname	callsign	speed	paclen	window	description"
+echo "$AX25PORT            $CALLSIGN-$SSID         9600    255     2       Direwolf port"
+} >> $AX25_CFGDIR/axports
 else
   echo "AX.25 port $AX25PORT already configured"
+fi
+
+# Set up a listening socket, for testing
+# Make it different than previous SSID
+if ((SSID < 15)) ; then
+   AX25DSSID=$((SSID+1))
+else
+   AX25DSSID=$((SSID-1))
+fi
+
+grep  "N0ONE" /etc/ax25/ax25d.conf >/dev/null
+if [ $? -eq 0 ] ; then
+   echo "ax25d not configured"
+   mv $AX25_CFGDIR/ax25d.conf $AX25_CFGDIR/ax25d.conf-dist
+   echo "Original ax25d.conf saved as ax25d.conf-dist"
+   # copy first 16 lines of file
+   sed -n '1,16p' $AX25_CFGDIR/ax25d.conf-dist >> $AX25_CFGDIR/ax25d.conf
+
+{
+echo "[$CALLSIGN-$AX25DSSID VIA $AX25PORT]"
+echo "NOCALL   * * * * * *  L"
+echo "default  * * * * * *  - root /usr/sbin/ttylinkd ttylinkd"
+} >> $AX25_CFGDIR/ax25d.conf
+
+else
+  echo "ax25d.conf already configured"
 fi
 
 exit 0
