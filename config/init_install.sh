c8f09f1502528e612a8cc0eda9e45ef74925a7b2
n7nix <basil@pacabunga.com>
2016-11-05 11:02:38 -0700

    Replace COMPASS_CFG notes with script.

--- config/init_install.sh
+++ config/init_install.sh
@@ -17,12 +17,14 @@ if [[ $EUID != 0 ]] ; then
    exit 1
 fi
 
+" === Check for updates"
 if [ "$UPDATE_NOW" = "true" ] ; then
   apt-get update
   apt-get upgrade
   apt-get install -y mg jed rsync build-essential autoconf automake libtool git libasound2-dev whois
 fi
 
+echo " === Verify not using default password"
 # is there even a user pi?
 ls /home | grep pi > /dev/null 2>&1
 if [ $? -eq 0 ] ; then
@@ -48,16 +50,107 @@ else
    echo "User pi NOT found"
 fi
 
-# Change hostname
-# mg /etc/hosts
-# mg /etc/hostname
+# Check hostname
+echo " === Verify hostname"
+HOSTNAME=$(cat /etc/hostname | tail -1)
+dbgecho "Current hostname: $HOSTNAME"
 
-HOSTNAME=$(cat /etc/hostname)
-dbgecho "Hostname: $HOSTNAME"
+if [ "$HOSTNAME" = "raspberrypi" ] || [ "$HOSTNAME" = "compass" ] ; then
+   # Change hostname
+   echo "Using default hostname, change it"
+   read HOSTNAME
+   echo "$HOSTNAME" > /etc/hostname
+fi
 
+
+# Get hostname again incase it was changed
+HOSTNAME=$(cat /etc/hostname | tail -1)
+
+# Make sure system host name can be resolved
+
+grep "127.0.1.1" /etc/hosts
+if [ $? -eq 0 ] ; then
+   # Found 127.0.1.1 entry
+   # Make sure hostnames match
+   HOSTNAME_CHECK=$(grep "127.0.1.1" /etc/hosts | awk {'print $2'})
+   if [ "$HOSTNAME" != "$HOSTNAME_CHECK" ] ; then
+      echo "host names do NOT match between /etc/hostname & /etc/hosts"
+      exit 1
+   fi
+else
+   # Add a 127.0.1.1 entry to /etc/hosts
+   sed -i '1i\'"127.0.1.1\t$HOSTNAME $HOSTNAME.localnet" /etc/hosts
+   if [ $? -ne 0 ] ; then
+      echo "Failed to modify /etchosts file"
+   fi
+fi
+
+echo " === Set time zone"
 DATETZ=$(date +%Z)
 dbgecho "TIme zone: $DATETZ"
 
 # dpkg-reconfigure tzdata
+
+echo " === Modify /boot/config.txt"
+# Add to bottom of file
+cat << EOT >> /boot/config
+
+# enable udrc
+dtoverlay=udrc
+force_turbo=1
+
+# Rotate lcd screen
+lcd_rotate=2
+
+#dtoverlay=udrc-boost-output
+EOT
+
+echo " enable modules"
+# Add to bottom of file
+cat << EOT >> /etc/modules
+
+i2c-dev
+ax25
+EOT
+
+echo "enable ax25 module"
+insmod /lib/modules/$(uname -r)/kernel/net/ax25/ax25.ko
+
+
+echo " === Install libax25, ax25apps & ax25tools"
+
+# Does source directory for ax25 utils exist?
+SRC_DIR="/usr/local/src/ax25"
+
+if [ ! -d $SRC_DIR ] ; then
+   mkdir -p $SRC_DIR
+   if [ $? -ne 0 ] ; then
+      echo "Problems creating source directory: $SRC_DIR"
+      exit 1
+   fi
+fi
+
+cd $SRC_DIR
+
+wget https://github.com/ve7fet/linuxax25/archive/master.zip
+unzip master.zip
+
+./updAX25.sh
+# libraries are installed in /usr/local/lib
+ldconfig
+cd ax25tools
+make installconf
+
+
+echo "=== Install direwolf"
+git clone https://www.github.com/wb2osz/direwolf
+cd direwolf
+make
+make install
+make install-conf
+# This failed
+make install-rpi
+
+cp /root/direwolf.conf /etc
 
 exit 0
