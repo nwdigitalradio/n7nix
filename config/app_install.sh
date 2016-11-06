c9db0be83401632b7c58ab200fd2e42e9fa76775
n7nix <basil@pacabunga.com>
2016-11-06 15:16:37 -0800

    First cut at installing apps after initial image config.

---
+++ config/app_install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+myname="`basename $0`"
+
+# make sure we're running as root
+if [[ $EUID != 0 ]] ; then
+   echo "$myname: Must be root"
+   exit 1
+fi
+
+# configure ax25
+source ../ax25/install.sh
+
+# configure direwolf
+source ../direwolf/install.sh
+
+# configure systemd
+pushd systemd
+source ./install.sh
+popd
+
+# install rmsgw
+source ../rmsgw/install.sh
+
+# configure rmsgw
+source ../rmsgw/config.sh
+
+echo "end app install: $myname"
+exit 0
