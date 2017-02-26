80c530fc6c8c7cb2a3a631a2247f16fef2a25ee4
n7nix <basil@pacabunga.com>
2017-02-26 14:39:47 -0800

    Add scripts to check access point install & config fixed ip.

---
+++ hostap/ap_status.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#
+SERVICELIST="hostapd dnsmasq"
+
+echo "Test if $SERVICELIST services have been started."
+for service_name in `echo ${SERVICELIST}` ; do
+echo
+echo "== status $service_name services =="
+   systemctl is-active $service_name >/dev/null
+   if [ "$?" = "0" ] ; then
+      echo "$service_name is running"
+   else
+      echo "$service_name is NOT running"
+   fi
+   systemctl status $service_name
+done
---
+++ hostap/fixed_ip.sh
@@ -0,0 +1,52 @@
+#! /bin/bash
+#
+# Set a fixed ip address
+#
+# Edit files:
+#  /etc/network/interfaces
+#  /etc/dhcpcd.conf
+
+lan_ipaddr="10.0.42.91"
+lan_router="10.0.42.1"
+
+wlan_ipaddr="10.0.44.1"
+
+cat <<EOT > /etc/network/interfaces
+source-directory /etc/network/interfaces.d
+
+auto lo
+iface lo inet loopback
+
+iface eth0 inet manual
+
+allow-hotplug wlan0
+iface wlan0 inet static
+  address $wlan_ipaddr
+  netmask 255.255.255.0
+  network 10.0.44.0
+  broadcast 10.0.44.255
+EOT
+
+cat <<EOT >> /etc/dhcpcd.conf
+
+interface eth0
+
+  static ip_address=$lan_ipaddr/24
+  static routers=$lan_router
+  static domain_name_servers=$lan_router
+
+interface wlan0
+
+  static ip_address=$wlan_ipaddr/24
+EOT
+echo "You are about to lose your SSH session"
+echo "Login in using lan address: $lan_ipaddr or wlan address: $wlan_ipaddr"
+systemctl is-enabled NetworkManager.service
+if [ $? -eq 0 ] ; then
+  systemctl disable NetworkManager.service
+fi
+service networking restart
+
+echo
+echo "Fixed IP address config FINISHED"
+echo
