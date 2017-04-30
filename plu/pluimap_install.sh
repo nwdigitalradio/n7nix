b09bc7b036dd36eda5d34de5669c04ea3b6bb476
n7nix <basil@pacabunga.com>
2017-04-30 10:43:57 -0700

    Add paclink-unix webb app start with systemd.

--- plu/pluimap_install.sh
+++ plu/pluimap_install.sh
@@ -8,7 +8,7 @@ DEBUG=1
 myname="`basename $0`"
 
 echo "$myname: paclink-unix with imap install"
-echo "$myname: Install paclink-unix, hostapd, dovecot & node.js"
+echo "$myname: Install paclink-unix, hostapd, dovecot, node.js & systemd files"
 # First install basic paclink-unix
 ./plu_install.sh
 
@@ -34,9 +34,28 @@ npm install -g websocket connect finalhandler serve-static
 npm install jquery
 cp node_modules/jquery/dist/jquery.min.js jquery.js
 
+echo "$myname: Install systemd files for paclink-unix web service."
+service_name="pluweb.service"
+# Setup systemd files for paclink-unix web server auto start
+if [ ! -f "/etc/systemd/system/$service_name" ] ; then
+   echo "File /etc/systemd/system/$service_name DOES NOT EXIST"
+   exit 1
+fi
+
+systemctl enable $service_name
+systemctl daemon-reload
+systemctl start $service_name
+
 echo
-echo "$myname: NOTE: You need to start paclink-unix web server"
-echo "$myname: As root cd /usr/local/src/paclink-unix/webapp then nodejs plu-server.js"
+echo "Check that $service_name is running."
+
+systemctl is-active $service_name >/dev/null
+if [ "$?" = "0" ] ; then
+   echo "$service_name is running!"
+else
+   echo "ERROR: $service_name did NOT start."
+fi
+
 echo
 echo "paclink-unix with imap, install script FINISHED"
 echo
