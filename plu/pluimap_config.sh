35b2455cde3d1010dd2a557eee1d617f599fd1d5
n7nix <basil@pacabunga.com>
2018-01-04 16:56:54 -0800

    start either stand alone or tracker plu systemd service.

--- plu/pluimap_config.sh
+++ plu/pluimap_config.sh
@@ -8,6 +8,14 @@ DEBUG=1
 scriptname="`basename $0`"
 UDR_INSTALL_LOGFILE="/var/log/udr_install.log"
 
+# ===== function systemservice_start
+
+function systemservice_start() {
+   systemctl enable $1
+   systemctl daemon-reload
+   systemctl start $1
+}
+
 # ===== Main
 
 echo "$scriptname: paclink-unix with imap configure"
@@ -28,30 +36,36 @@ source ./imapserv_config.sh
 popd
 
 # Set up a host access point for remote operation
+echo "=== Config hostap"
 pushd ../hostap
 source ./hostap_config.sh
 popd
 
-echo "$scriptname: Install systemd files for paclink-unix web service."
-service_name="pluweb.service"
+echo "$scriptname: Start systemd service for paclink-unix web service."
+service_name=
 # Setup systemd files for paclink-unix web server auto start
-if [ ! -f "/etc/systemd/system/$service_name" ] ; then
-   echo "File /etc/systemd/system/$service_name DOES NOT EXIST"
-   exit 1
+if [ -f "/etc/systemd/system/pluweb.service" ] ; then
+   service_name="pluweb.service"
+   systemservice_start "$service_name"
+elif [ -f "/etc/systemd/system/tracker.service" ] ; then
+   service_name="tracker.service"
+   systemservice_start "$service_name"
+else
+   echo
+   echo "No systemd service file found for starting paclink-unix web app."
+   echo
 fi
 
-systemctl enable $service_name
-systemctl daemon-reload
-systemctl start $service_name
+if [ ! -z $service_name ] ; then
+   echo
+   echo "Check that $service_name is running."
 
-echo
-echo "Check that $service_name is running."
-
-systemctl is-active $service_name >/dev/null
-if [ "$?" = "0" ] ; then
-   echo "$service_name is running!"
-else
-   echo "ERROR: $service_name did NOT start."
+   systemctl is-active $service_name >/dev/null
+   if [ "$?" = "0" ] ; then
+      echo "$service_name is running!"
+   else
+      echo "ERROR: $service_name did NOT start."
+   fi
 fi
 
 echo "$(date "+%Y %m %d %T %Z"): $scriptname: plu with imap config script FINISHED" >> $UDR_INSTALL_LOGFILE
