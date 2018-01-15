6e02da98aaad029023e2c794fd86d79e07244f6d
n7nix <basil@pacabunga.com>
2018-01-15 10:53:50 -0800

    Check running as root & unmount flash device.

--- kernbuild/flashit.sh
+++ kernbuild/flashit.sh
@@ -1,16 +1,23 @@
 #/bin/bash
-flash_dev="/dev/sde"
 
+flash_dev="/dev/sde"
 img_date="2018-01-11"
 
-if mount | grep -q $flash_dev; then
-   echo "$flash_dev is mounted, need to unmount"
-   exit
+# must run as root
+if [[ $EUID -ne 0 ]]; then
+  echo "*** Run as root" 2>&1
+  exit 1
 fi
 
-time dd if=${img_date}-compass.img of=/dev/sde bs=1M status=progress
+if mount | grep -q $flash_dev; then
+   echo "$flash_dev is mounted ... unmounting"
+   umount ${flash_dev}1
+   umount ${flash_dev}2
+fi
 
-mount /dev/sde1 /mnt/fat32
+time dd if=${img_date}-compass.img of=$flash_dev bs=1M status=progress
+
+mount ${flash_dev}1 /mnt/fat32
 touch /mnt/fat32/ssh
 sync
 umount /mnt/fat32
