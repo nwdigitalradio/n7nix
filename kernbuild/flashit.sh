47e8453009d830a0033296e3355f4a6d1acd2a82
n7nix <basil@pacabunga.com>
2018-02-03 09:27:15 -0800

    Add comments, use latest wilderness kernel.

--- kernbuild/flashit.sh
+++ kernbuild/flashit.sh
@@ -1,12 +1,56 @@
 #/bin/bash
+#
+# flashit.sh
+# - Unzips & writes to flash a compass image file
+#
+# No command line args used
+# Set up 3 variables before using this script
+# - flash_dev flash device
+# - img_date  image date (defaults to todays date)
+# - kernlite  true or false for including window manager
+#
+# If DEBUG is defined no flash writing will occur
+DEBUG=1
 
+img_date=
 flash_dev="/dev/sde"
-img_date="2018-01-11"
+kernlite="true"
 
 # must run as root
-if [[ $EUID -ne 0 ]]; then
+if [ -z $DEBUG ] && [[ $EUID -ne 0 ]]; then
   echo "*** Run as root" 2>&1
   exit 1
+fi
+
+# Create flash file name
+if [ -z "$img_date" ] ; then
+   img_date="$(date "+%Y-%m-%d")"
+fi
+
+flashfile_name="${img_date}-compass"
+
+# Is lite or full image required
+if [ "$kernlite" == "true" ] ; then
+   flashfile_name="${img_date}-compass-lite"
+fi
+
+# Does the image file already exist?
+if [ ! -f ${flashfile_name}.img ] ; then
+   # Does the zipped image file already exist?
+   if [ -f "image_${flashfile_name}.zip" ] ; then
+      echo "Unzipping file: image_${flashfile_name}.zip ... please wait"
+      unzip image_${flashfile_name}.zip
+   else
+      # Download the request compass image file
+      echo "Downloading compass image file: image_${flashfile_name}.zip ... please wait"
+      wget -qt 3 https://nwdr-compass-images.s3.amazonaws.com/image_${flashfile_name}.zip
+      if [ $? -ne 0 ] ; then
+         echo "Problem encountered downloading compass image file: image_${flashfile_name}.zip"
+         exit 1
+      fi
+      echo "Unzipping file: image_${flashfile_name}.zip"
+      unzip image_${flashfile_name}.zip
+   fi
 fi
 
 if mount | grep -q $flash_dev; then
@@ -15,7 +59,14 @@ if mount | grep -q $flash_dev; then
    umount ${flash_dev}2
 fi
 
-time dd if=${img_date}-compass.img of=$flash_dev bs=1M status=progress
+echo "Copying image file: ${flashfile_name}.img, size: $(du -h ${flashfile_name}.img | cut -f1)"
+
+if [ ! -z $DEBUG ] ; then
+  echo "Exiting on DEBUG"
+  exit
+fi
+
+time dd if=${flashfile_name}.img of=$flash_dev bs=1M status=progress
 
 mount ${flash_dev}1 /mnt/fat32
 touch /mnt/fat32/ssh
