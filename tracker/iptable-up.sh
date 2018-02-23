ff71b90e8ec27a60b8640b9375aa8e0c97daf424
n7nix <basil@pacabunga.com>
2018-02-23 15:15:56 -0800

    Add second ax.25 interface.

--- tracker/iptable-up.sh
+++ tracker/iptable-up.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 iptables -A OUTPUT -o ax0 -d 224.0.0.22 -p igmp -j DROP
 iptables -A OUTPUT -o ax0 -d 224.0.0.251 -p udp -m udp --dport 5353 -j DROP
+iptables -A OUTPUT -o ax1 -d 224.0.0.22 -p igmp -j DROP
+iptables -A OUTPUT -o ax1 -d 224.0.0.251 -p udp -m udp --dport 5353 -j DROP
+
