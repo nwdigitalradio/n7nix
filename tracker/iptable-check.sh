bf3c5903466ab1d093531ac65c121d03e91e48b2
n7nix <basil@pacabunga.com>
2018-02-24 13:49:55 -0800

    Add check for running as root, check for existences of ax25 rules.

--- tracker/iptable-check.sh
+++ tracker/iptable-check.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
+#
+SU=
+
+# Check for required iptables files
+#
+IPTABLES_FILES="/etc/iptables/rules.ipv4.ax25 /lib/dhcpcd/dhcpcd-hooks/70-ipv4.ax25"
+for ipt_file in `echo ${IPTABLES_FILES}` ; do
+
+   if [ -f $ipt_file ] ; then
+      echo "iptables file: $ipt_file exists"
+   else
+      echo "Need to create iptables file: $ipt_file"
+   fi
+done
+
+# Be sure we're running as root
+if [[ $EUID != 0 ]] ; then
+   SU="sudo"
+fi
+
+# List iptables rules
+#
 # -L list: List all rules in all chains
 # -v verbose output
 # -n numeric: IP addresses & port numbers are printed in numeric format
 # -x exact: display exact value of the packet & byte counters instead
 #    of rounded number
-iptables -L -nvx
+$SU iptables -L -nvx
