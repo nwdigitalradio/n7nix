b503a9d2b8c6adb2d1c3652f88dfa0259a84bf5e
n7nix <basil@pacabunga.com>
2018-05-03 12:38:20 -0700

    Debug for ax25-stop/ax25-start problem.

---
+++ debug/tup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# tup.sh
+#
+# Test ax25-stop, ax25-start scripts
+
+scriptname="`basename $0`"
+
+# Be sure we're running as root
+if [[ $EUID != 0 ]] ; then
+   echo "Must be root"
+   exit 1
+fi
+
+echo "$scriptname start"
+
+{
+/home/pi/bin/sysver.sh
+echo
+echo "ax25-stop at $(date)"
+
+/home/pi/bin/ax25-stop
+
+echo
+echo "ax25-start at $(date)"
+
+/home/pi/bin/ax25-start
+journalctl -u ax25dev.service
+} > tupax25.log
+echo "$scriptname finished"
+exit 0
