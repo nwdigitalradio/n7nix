08d104b4b6d12a6396336401e10a5daa1c6380fd
n7nix <basil@pacabunga.com>
2018-05-09 09:08:27 -0700

    Add 2 more models & a catch all.

--- systemd/bin/piver.sh
+++ systemd/bin/piver.sh
@@ -13,10 +13,10 @@ piver="$(echo -e "${piver##*:}" | tr -d '[[:space:]]')"
 
 case $piver in
 a01040)
-   echo " Pi 2 Model B Mfg by Unknown"
+   echo " Pi 2 Model B Mfg by Sony UK"
 ;;
 a01041)
-   echo " Pi 2 Model B Mfg by Sony"
+   echo " Pi 2 Model B Mfg by Sony UK"
 ;;
 a21041)
    echo " Pi 2 Model B Mfg by Embest"
@@ -24,13 +24,24 @@ a21041)
 a22042)
    echo " Pi 2 Model B with BCM2837 Mfg by Embest"
 ;;
-a02082 | a32082)
-   echo " Pi 3 Model B Mfg by Sony"
+a02082)
+   echo " Pi 3 Model B Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 a22082)
    echo " Pi 3 Model B Mfg by Embest"
    HAS_WIFI=true
+;;
+a32082)
+   echo " Pi 3 Model B Mfg by Sony Japan"
+   HAS_WIFI=true
+;;
+a020d3)
+   echo " Pi 3 Model B+ Mfg by Sony UK"
+   HAS_WIFI=true
+;;
+*)
+   echo "Unknown pi version: $piver"
 ;;
 esac
 
