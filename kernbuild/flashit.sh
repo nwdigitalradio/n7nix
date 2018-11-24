26499bc5e9b11b23a2373d251fe88d6b8333de3a
n7nix <basil@pacabunga.com>
2018-11-24 12:53:57 -0800

    Update for naming images that do NOT come from Wilderness.

--- kernbuild/flashit.sh
+++ kernbuild/flashit.sh
@@ -16,93 +16,112 @@ flash_dev="/dev/sdf"
 
 # Build image name from these variables
 # Need this for raspbian
-img_date="2018-07-13"
+img_date="2018-11-18"
 kernlite="true"
-
-# Choose one for compass or raspbian
-#fs_source="compass-4GB"
-#fs_source="compass"
-fs_source="raspbian-stretch"
+custom_image="false"
+custom_image_name=
 
 # must run as root
 if [ -z $DEBUG ] && [[ $EUID -ne 0 ]]; then
-  echo "*** Run as root" 2>&1
-  exit 1
+    echo "*** Run as root" 2>&1
+    exit 1
 fi
 
-# Used by Compass, since it has daily builds
-# Create flash file name
-if [ -z "$img_date" ] ; then
-   img_date="$(date "+%Y-%m-%d")"
+# Check if there are any args on command line
+if (( $# != 0 )) ; then
+   custom_image="true"
+   custom_image_name="udrc_eeprom"
 fi
 
-# Is lite or full image required
-   flashfile_name="${img_date}-${fs_source}"
-if [ "$kernlite" == "true" ] ; then
-   flashfile_name="${img_date}-${fs_source}-lite"
-fi
+if [ "$custom_image" != "true" ] ; then
+    # Choose one for compass or raspbian
+    #fs_source="compass-4GB"
+    fs_source="compass"
+    #fs_source="raspbian-stretch"
 
-zipfile_name="${flashfile_name}.zip"
-if [ "${fs_source:0:7}" == "compass" ] ; then
-   zipfile_name="image_${flashfile_name}.zip"
-fi
+    # Used by Compass, since it has daily builds
+    # Create flash file name
+    if [ -z "$img_date" ] ; then
+        img_date="$(date "+%Y-%m-%d")"
+    fi
 
-echo "Using base filename: $flashfile_name, zip filename: $zipfile_name"
+    # Is lite or full image required
+    flashfile_name="${img_date}-${fs_source}"
+    if [ "$kernlite" == "true" ] ; then
+        flashfile_name="${img_date}-${fs_source}-lite"
+    fi
 
-# Does the image file already exist?
-if [ ! -f "${flashfile_name}.img" ] ; then
-   # Does the zipped image file already exist?
-    if [ -f "${zipfile_name}" ] ; then
-      echo "Unzipping file: ${zipfile_name} ... please wait"
-      unzip ${zipfile_name}
-   else
-      # Download the requested image file
-      if [ "${fs_source:0:7}" == "compass" ] ; then
-         # Download a compass image
-         echo "Downloading compass image file: ${zipfile_name} ... please wait"
-         wget -qt 3 https://nwdr-compass-images.s3.amazonaws.com/${zipfile_name}
-      else
-         #Download a raspbian image
-         echo "Downloading raspbian image file: ${zipfile_name} ... please wait"\
-         raspbianfile_name="raspbian_latest"
-         if [ "$kernlite" == "true" ] ; then
-            raspbianfile_name="raspbian_lite_latest"
-         fi
-         wget -qt 3 https://downloads.raspberrypi.org/${raspbianfile_name}
-      fi
-      # Check status of download
-      if [ $? -ne 0 ] ; then
-         echo "Problem encountered downloading image file: ${zipfile_name}"
-         exit 1
-      fi
-      echo "Unzipping file: ${zipfile_name}"
-      unzip ${zipfile_name}
-   fi
+    zipfile_name="${flashfile_name}.zip"
+    if [ "${fs_source:0:7}" == "compass" ] ; then
+        zipfile_name="image_${flashfile_name}.zip"
+    fi
+
+    echo "Using base filename: $flashfile_name, zip filename: $zipfile_name"
+
+    # Does the image file already exist?
+    if [ ! -f "${flashfile_name}.img" ] ; then
+        # Does the zipped image file already exist?
+        if [ -f "${zipfile_name}" ] ; then
+            echo "Unzipping file: ${zipfile_name} ... please wait"
+            unzip ${zipfile_name}
+        else
+            # Download the requested image file
+            if [ "${fs_source:0:7}" == "compass" ] ; then
+                # Download a compass image
+                echo "Downloading compass image file: ${zipfile_name} ... please wait"
+                wget -qt 3 https://nwdr-compass-images.s3.amazonaws.com/${zipfile_name}
+            else
+                #Download a raspbian image
+                echo "Downloading raspbian image file: ${zipfile_name} ... please wait"\
+                raspbianfile_name="raspbian_latest"
+                if [ "$kernlite" == "true" ] ; then
+                    raspbianfile_name="raspbian_lite_latest"
+                fi
+                wget -qt 3 https://downloads.raspberrypi.org/${raspbianfile_name}
+            fi
+            # Check status of download
+            if [ $? -ne 0 ] ; then
+                echo "Problem encountered downloading image file: ${zipfile_name}"
+                exit 1
+            fi
+            echo "Unzipping file: ${zipfile_name}"
+            unzip ${zipfile_name}
+        fi
+    else
+        echo "Flash image file: ${flashfile_name}.img exists, using it"
+    fi
 else
-   echo "Flash image file: ${flashfile_name}.img exists, using it"
+
+    flashfile_name="$custom_image_name"
+    if [ -f "${flashfile_name}.img" ] ; then
+	echo "Flash image file: ${flashfile_name}.img exists, using it"
+    else
+	echo "Flash image file: ${flashfile_name}.img does NOTexist, exiting"
+    fi
 fi
 
 flash_device=${flash_dev}1
 mntpnt=$(findmnt -n $flash_device | cut -d ' ' -f1)
 if [ ! -z "$mntpnt" ] ; then
-   echo "$flash_device is mounted at $mntpnt ... unmounting"
-   umount $mntpnt
+    echo "$flash_device is mounted at $mntpnt ... unmounting"
+    umount $mntpnt
 fi
 
 flash_device=${flash_dev}2
 mntpnt=$(findmnt -n $flash_device | cut -d ' ' -f1)
 if [ ! -z "$mntpnt" ] ; then
-   echo "$flash_device is mounted at $mntpnt ... unmounting"
-   umount $mntpnt
+    echo "$flash_device is mounted at $mntpnt ... unmounting"
+    umount $mntpnt
 fi
 
 echo "Copying image file: ${flashfile_name}.img, size: $(du -h ${flashfile_name}.img | cut -f1)"
 
 if [ ! -z $DEBUG ] ; then
-  echo "Exiting on DEBUG"
-  exit
+    echo "Exiting on DEBUG"
+    exit
 fi
 
+# U.S. Department of Defense Computer Forensics Lab version of dd
 time dcfldd if=${flashfile_name}.img of=$flash_dev bs=4M status=progress
 sync
 
