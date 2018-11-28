b67a3623c47fb16908bf0a94655a1fbffc9f7b01
n7nix <basil@pacabunga.com>
2018-11-27 16:02:36 -0800

    Setup image installer to pass callsign to other installers.

--- config/image_install.sh
+++ config/image_install.sh
@@ -9,6 +9,8 @@ UDR_INSTALL_LOGFILE="/var/log/udr_install.log"
 imapinstall="false"
 USER=
 
+function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
+
 # ===== function get_user
 
 function get_user() {
@@ -41,27 +43,13 @@ function check_user() {
    dbgecho "using USER: $USER"
 }
 
-# ==== function check_user
-# verify user name is legit
-
-function check_user() {
-   userok=false
-   dbgecho "$scriptname: Verify user name: $USER"
-   for username in $USERLIST ; do
-      if [ "$USER" = "$username" ] ; then
-         userok=true;
-      fi
-   done
-
-   if [ "$userok" = "false" ] ; then
-      echo "User name ($USER) does not exist,  must be one of: $USERLIST"
-      exit 1
-   fi
-
-   dbgecho "using USER: $USER"
-}
-
 # ===== main
+
+# Be sure we're running as root
+if [[ $EUID != 0 ]] ; then
+   echo "$scriptname: Must be root"
+   exit 1
+fi
 
 echo "$(date "+%Y %m %d %T %Z"): $scriptname: image install script START" >> $UDR_INSTALL_LOGFILE
 echo
@@ -71,29 +59,28 @@ if (( $# != 0 )) ; then
    USER="$1"
 fi
 
-# Be sure we're running as root
-if [[ $EUID != 0 ]] ; then
-   echo "$scriptname: Must be root"
-   exit 1
-fi
-
 echo "$scriptname: Install direwolf & ax25 files"
 ./core_install.sh
 
 
 echo "$scriptname: Install RMS Gateway"
-# Running as root?
-if [[ $EUID == 0 ]] ; then
-    echo
-    echo "Not required to run RMSGW install script as root ...."
+# Get list of users with home directories
+USERLIST="$(ls /home)"
+USERLIST="$(echo $USERLIST | tr '\n' ' ')"
 
+# Check if user name was supplied on command line
+if [ -z "$USER" ] ; then
+    # prompt for call sign & user name
+    # Check if there is only a single user on this system
     get_user
-    check_user
-
-    pushd ../rmsgw
-    sudo -u "$USER" source ./install.sh
-    popd > /dev/null
 fi
+# Verify user name
+check_user
+
+# run RMSGW install script as user other than root
+pushd ../rmsgw
+sudo -u "$USER" ./install.sh
+popd > /dev/null
 
 pushd ../plu
 if [ "$imapinstall" = "true" ] ; then
