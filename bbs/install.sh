903f7239589d5db97bfaa4bdf1141726859cfbd3
n7nix <basil@pacabunga.com>
2018-12-04 15:05:22 -0800

    Handle user name as first arg.

--- bbs/install.sh
+++ bbs/install.sh
@@ -8,9 +8,44 @@ num_cores=$(nproc --all)
 SRC_DIR="/usr/local/src/"
 scriptname="`basename $0`"
 UDR_INSTALL_LOGFILE="/var/log/udr_install.log"
+USER=
 
 PKG_REQUIRE=""
 PKG_REQUIRE_X11="libx11-dev ligxt-dev libxext-dev libxpm-dev lesstif2-dev"
+
+function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
+
+# ===== function get_user
+
+function get_user() {
+   # Check if there is only a single user on this system
+   if (( `ls /home | wc -l` == 1 )) ; then
+      USER=$(ls /home)
+   else
+      echo "Enter user name ($(echo $USERLIST | tr '\n' ' ')), followed by [enter]:"
+      read -e USER
+   fi
+}
+
+# ==== function check_user
+# verify user name is legit
+
+function check_user() {
+   userok=false
+   dbgecho "$scriptname: Verify user name: $USER"
+   for username in $USERLIST ; do
+      if [ "$USER" = "$username" ] ; then
+         userok=true;
+      fi
+   done
+
+   if [ "$userok" = "false" ] ; then
+      echo "User name ($USER) does not exist,  must be one of: $USERLIST"
+      exit 1
+   fi
+
+   dbgecho "using USER: $USER"
+}
 
 # ===== main
 echo -e "\n\tInstall FBB BBS\n"
@@ -22,16 +57,35 @@ if [[ $EUID == 0 ]] ; then
    exit 1
 fi
 
+# Check for any arguments
+if (( $# != 0 )) ; then
+   USER="$1"
+fi
+
+# Get list of users with home directories
+USERLIST="$(ls /home)"
+USERLIST="$(echo $USERLIST | tr '\n' ' ')"
+
+# Check if user name was supplied on command line
+if [ -z "$USER" ] ; then
+    # prompt for call sign & user name
+    # Check if there is only a single user on this system
+    get_user
+fi
+# Verify user name
+check_user
+
 # check if required packages are installed
 dbgecho "Check packages: $PKG_REQUIRE"
 
 echo "=== Install fbb version $BBS_VER from source using $num_cores cores"
 
 cd "$SRC_DIR"
-wget https://sourceforge.net/projects/linfbb/files/latest/download/fbb-$BBS_VER.tar.bz2
+sudo wget https://sourceforge.net/projects/linfbb/files/latest/download/fbb-$BBS_VER.tar.bz2
 echo "wget ret: $?"
-tar xjvf fbb-$BBS_VER.tar.bz2
+sudo tar xjvf fbb-$BBS_VER.tar.bz2
 echo "wget ret: $?"
+sudo chown -R $USER:$USER fbb-$BBS_VER/
 cd fbb-$BBS_VER/
 ./configure
 echo "configure ret: $?"
--- yaac/install.sh
+++ yaac/install.sh
@@ -5,6 +5,10 @@
 
 SRC_DIR="/usr/local/src/"
 YAAC_SRC_DIR=$SRC_DIR/yaac
+scriptname="`basename $0`"
+UDR_INSTALL_LOGFILE="/var/log/udr_install.log"
+USER=
+
 PKG_REQUIRE="openjdk-8-jre librxtx-java unzip"
 
 function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
@@ -14,6 +18,38 @@ function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
 function is_pkg_installed() {
 
 return $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed" >/dev/null 2>&1)
+}
+
+# ===== function get_user
+
+function get_user() {
+   # Check if there is only a single user on this system
+   if (( `ls /home | wc -l` == 1 )) ; then
+      USER=$(ls /home)
+   else
+      echo "Enter user name ($(echo $USERLIST | tr '\n' ' ')), followed by [enter]:"
+      read -e USER
+   fi
+}
+
+# ==== function check_user
+# verify user name is legit
+
+function check_user() {
+   userok=false
+   dbgecho "$scriptname: Verify user name: $USER"
+   for username in $USERLIST ; do
+      if [ "$USER" = "$username" ] ; then
+         userok=true;
+      fi
+   done
+
+   if [ "$userok" = "false" ] ; then
+      echo "User name ($USER) does not exist,  must be one of: $USERLIST"
+      exit 1
+   fi
+
+   dbgecho "using USER: $USER"
 }
 
 #
@@ -33,6 +69,21 @@ if (( $# != 0 )) ; then
 else
     echo "$scriptname: Must supply user name as command line argument"
     exit 1
+fi
+# Check for any arguments
+if (( $# != 0 )) ; then
+   USER="$1"
+fi
+
+# Get list of users with home directories
+USERLIST="$(ls /home)"
+USERLIST="$(echo $USERLIST | tr '\n' ' ')"
+
+# Check if user name was supplied on command line
+if [ -z "$USER" ] ; then
+    # prompt for call sign & user name
+    # Check if there is only a single user on this system
+    get_user
 fi
 
 # Verify user name
