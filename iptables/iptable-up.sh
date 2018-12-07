208f4e4bb4f88b8f583aecbf5a7cd9a7556b82ec
n7nix <basil@pacabunga.com>
2018-12-07 15:10:33 -0800

    Add dropping Simple Service Discovery Protocol.

--- iptables/iptable-up.sh
+++ iptables/iptable-up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 iptables -A OUTPUT -o ax0 -d 224.0.0.22 -p igmp -j DROP
 iptables -A OUTPUT -o ax0 -d 224.0.0.251 -p udp -m udp --dport 5353 -j DROP
+iptables -A OUTPUT -o ax0 -d 239.255.255.250 -p udp -m udp  -j DROP
 iptables -A OUTPUT -o ax1 -d 224.0.0.22 -p igmp -j DROP
 iptables -A OUTPUT -o ax1 -d 224.0.0.251 -p udp -m udp --dport 5353 -j DROP
-
+iptables -A OUTPUT -o ax1 -d 239.255.255.250 -p udp -m udp  -j DROP
