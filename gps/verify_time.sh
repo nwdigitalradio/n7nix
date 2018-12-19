cc88f972a3d992e6c3e7272ca0e5ce9fbc817e52
n7nix <basil@pacabunga.com>
2018-12-18 22:09:57 -0800

    Add script to collect data on the chrony daemon.

---
+++ gps/verify_time.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Collect a bunch of information about ntp time condition
+
+echo -e "\n\t$(tput setaf 4)timedatectl$(tput setaf 7)\n"
+sudo timedatectl
+echo -e "\n\t$(tput setaf 4)chronyc sources$(tput setaf 7)\n"
+chronyc sources
+echo -e "\n\t$(tput setaf 4)chronyc sourcestats$(tput setaf 7)\n"
+chronyc sourcestats
+echo -e "\n\t$(tput setaf 4)chronyc tracking$(tput setaf 7)\n"
+chronyc tracking
+echo -e "\n\t$(tput setaf 4)chronyc activity$(tput setaf 7)\n"
+chronyc activity
+echo -e "\n\t$(tput setaf 4)systemctl status$(tput setaf 7)\n"
+systemctl status chronyd
