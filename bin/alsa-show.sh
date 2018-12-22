fb0beffc4e99678837d020d8651f0264bee40c54
n7nix <basil@pacabunga.com>
2018-12-21 16:22:36 -0800

    Display right & left inputs on a single line, add debug option.

--- bin/alsa-show.sh
+++ bin/alsa-show.sh
@@ -3,19 +3,10 @@
 # Uncomment this statement for debug echos
 # DEBUG=1
 
+scriptname="`basename $0`"
+
 # Default card name
 CARD="udrc"
-
-# amixer -c $CARD get 'ADC Level'
-#amixer -c $CARD get 'LO Driver Gain'
-CONTROL_LIST="'ADC Level''LO Drive Gain' 'PCM'"
-
-# parse any command line options
-# arg1 allows setting the CARD, default is "udrc"
-if [[ $# -ne 0 ]] ; then
-   CARD="$1"
-   echo "Setting sound card to: $CARD"
-fi
 
 # ===== function dbgecho
 function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
@@ -39,7 +30,7 @@ function audio_display_ctrl() {
 function input_display_ctrl() {
     alsa_ctrl="$1"
     CTRL_STR="$(amixer -c $CARD get \""$alsa_ctrl"\")"
-    dbgecho "$alsa_ctrl: $CTRL_STR"
+#    dbgecho "$alsa_ctrl: $CTRL_STR"
     CTRL_VAL=$(amixer -c $CARD get \""$alsa_ctrl"\" | grep -i -m 1 "Item0:" | cut -d ':' -f2)
     # Remove preceeding white space
     CTRL_VAL="$(sed -e 's/^[[:space:]]*//' <<<"$CTRL_VAL")"
@@ -47,6 +38,54 @@ function input_display_ctrl() {
     CTRL_VAL=${CTRL_VAL%\'}
     CTRL_VAL=${CTRL_VAL#\'}
 }
+
+# ===== Display program help info
+
+function usage () {
+	(
+	echo "Usage: $scriptname [-c card_name][-d][-h]"
+        echo "    -c card_name, default=udrc"
+        echo "    -d switch to turn on verbose debug display"
+        echo "    -h display this message."
+        echo
+	) 1>&2
+	exit 1
+}
+
+# ===== main
+
+# amixer -c $CARD get 'ADC Level'
+# amixer -c $CARD get 'LO Driver Gain'
+CONTROL_LIST="'ADC Level' 'LO Drive Gain' 'PCM'"
+
+# parse any command line options
+while [[ $# -gt 0 ]] ; do
+
+    key="$1"
+    case $key in
+        -d)
+            echo "Turning on debug"
+            DEBUG=1
+        ;;
+        -c)
+
+            CARD="$2"
+            shift # past value
+            echo "Setting card name to $CARD"
+        ;;
+        -h)
+            usage
+            exit 0
+        ;;
+        *)
+            echo "Undefined argument: $key"
+            usage
+            exit 1
+        ;;
+    esac
+    shift # past argument or value
+done
+
 
 control="PCM"
 audio_display_ctrl "$control"
@@ -62,16 +101,56 @@ printf "%s  L:%s\tR:%s\n" "$control" $CTRL_VAL_L $CTRL_VAL_R
 
 control="IN1_L to Left Mixer Positive Resistor"
 input_display_ctrl "$control"
+CTRL_IN1_L="$CTRL_VAL"
+
+control="IN1_R to Right Mixer Positive Resistor"
+input_display_ctrl "$control"
+CTRL_IN1_R="$CTRL_VAL"
+
+control="IN2_L to Left Mixer Positive Resistor"
+input_display_ctrl "$control"
+CTRL_IN2_L="$CTRL_VAL"
+
+control="IN2_R to Right Mixer Positive Resistor"
+input_display_ctrl "$control"
+CTRL_IN2_R="$CTRL_VAL"
+
+
+control="IN1"
+strlen=${#CTRL_IN1_L}
+if ((strlen < 4)) ; then
+    printf "%s\t\tL:[%s]\t\tR:[%s]\n" "$control" "$CTRL_IN1_L" "$CTRL_IN1_R"
+else
+    printf "%s\t\tL:[%s]\tR:[%s]\n" "$control" "$CTRL_IN1_L" "$CTRL_IN1_R"
+fi
+
+control="IN2"
+strlen=${#CTRL_IN2_L}
+if ((strlen < 4)) ; then
+    printf "%s\t\tL:[%s]\t\tR:[%s]\n" "$control" "$CTRL_IN2_L" "$CTRL_IN2_R"
+else
+    printf "%s\t\tL:[%s]\tR:[%s]\n" "$control" "$CTRL_IN2_L" "$CTRL_IN2_R"
+fi
+
+
+if [ ! -z "$DEBUG" ] ; then
+control="IN1_L to Left Mixer Positive Resistor"
+input_display_ctrl "$control"
+CTRL_IN1_L="$CTRL_VAL"
 printf "%s\t%s\n" "$control" "$CTRL_VAL"
 
 control="IN1_R to Right Mixer Positive Resistor"
 input_display_ctrl "$control"
+CTRL_IN1_R="$CTRL_VAL"
 printf "%s\t%s\n" "$control" "$CTRL_VAL"
 
 control="IN2_L to Left Mixer Positive Resistor"
 input_display_ctrl "$control"
+CTRL_IN2_L="$CTRL_VAL"
 printf "%s\t%s\n" "$control" $CTRL_VAL
 
 control="IN2_R to Right Mixer Positive Resistor"
 input_display_ctrl "$control"
+CTRL_IN2_R="$CTRL_VAL"
 printf "%s\t%s\n" "$control" $CTRL_VAL
+fi
