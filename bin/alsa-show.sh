ddda6032df1a1f1f4dced5fddc5e61683aef0dff
n7nix <basil@pacabunga.com>
2019-01-02 08:39:44 -0800

    Separate ALSA controls for transmit & receive.

--- bin/alsa-show.sh
+++ bin/alsa-show.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 # Display alsa controls that are interesting
 # Uncomment this statement for debug echos
 # DEBUG=1
@@ -25,9 +26,9 @@ function audio_display_ctrl() {
    dbgecho "$alsa_ctrl: Right $PCM_VAL"
 }
 
-# ===== function input_display_ctrl
+# ===== function display_ctrl
 
-function input_display_ctrl() {
+function display_ctrl() {
     alsa_ctrl="$1"
     CTRL_STR="$(amixer -c $CARD get \""$alsa_ctrl"\")"
 #    dbgecho "$alsa_ctrl: $CTRL_STR"
@@ -86,33 +87,55 @@ while [[ $# -gt 0 ]] ; do
     shift # past argument or value
 done
 
-
-control="PCM"
-audio_display_ctrl "$control"
-printf "%s\t        L:%s\tR:%s\n" "$control" $CTRL_VAL_L $CTRL_VAL_R
-
-control="ADC Level"
-audio_display_ctrl "$control"
-printf "%s\tL:%s\tR:%s\n" "$control" $CTRL_VAL_L $CTRL_VAL_R
+echo " ===== ALSA Controls for Radio Tansmit ====="
 
 control="LO Driver Gain"
 audio_display_ctrl "$control"
 printf "%s  L:%s\tR:%s\n" "$control" $CTRL_VAL_L $CTRL_VAL_R
 
+control="LO Playback Common Mode"
+display_ctrl "$control"
+# echo "DEBUG: CTRL_VAL: $CTRL_VAL"
+# Shorten control string for display
+control="LO Playback CM"
+printf "%s\t[%s]\n" "$control" "$CTRL_VAL"
+
+control="PCM"
+audio_display_ctrl "$control"
+printf "%s\t        L:%s\tR:%s\n" "$control" $CTRL_VAL_L $CTRL_VAL_R
+
+control="DAC Left Playback PowerTune"
+display_ctrl "$control"
+CTRL_PTM_L="$CTRL_VAL"
+
+control="DAC Right Playback PowerTune"
+display_ctrl "$control"
+CTRL_PTM_R="$CTRL_VAL"
+# Shorten control string for display
+control="DAC Playback PT"
+printf "%s\tL:[%s]\tR:[%s]\n" "$control" "$CTRL_PTM_L" "$CTRL_PTM_R"
+
+echo
+echo " ===== ALSA Controls for Radio Receive ====="
+
+control="ADC Level"
+audio_display_ctrl "$control"
+printf "%s\tL:%s\tR:%s\n" "$control" $CTRL_VAL_L $CTRL_VAL_R
+
 control="IN1_L to Left Mixer Positive Resistor"
-input_display_ctrl "$control"
+display_ctrl "$control"
 CTRL_IN1_L="$CTRL_VAL"
 
 control="IN1_R to Right Mixer Positive Resistor"
-input_display_ctrl "$control"
+display_ctrl "$control"
 CTRL_IN1_R="$CTRL_VAL"
 
 control="IN2_L to Left Mixer Positive Resistor"
-input_display_ctrl "$control"
+display_ctrl "$control"
 CTRL_IN2_L="$CTRL_VAL"
 
 control="IN2_R to Right Mixer Positive Resistor"
-input_display_ctrl "$control"
+display_ctrl "$control"
 CTRL_IN2_R="$CTRL_VAL"
 
 control="IN1"
@@ -131,25 +154,31 @@ else
     printf "%s\t\tL:[%s]\tR:[%s]\n" "$control" "$CTRL_IN2_L" "$CTRL_IN2_R"
 fi
 
-
 if [ ! -z "$DEBUG" ] ; then
-control="IN1_L to Left Mixer Positive Resistor"
-input_display_ctrl "$control"
-CTRL_IN1_L="$CTRL_VAL"
-printf "%s\t%s\n" "$control" "$CTRL_VAL"
+    control="IN1_L to Left Mixer Positive Resistor"
+    display_ctrl "$control"
+    CTRL_IN1_L="$CTRL_VAL"
+    printf "%s\t%s\n" "$control" "$CTRL_VAL"
 
-control="IN1_R to Right Mixer Positive Resistor"
-input_display_ctrl "$control"
-CTRL_IN1_R="$CTRL_VAL"
-printf "%s\t%s\n" "$control" "$CTRL_VAL"
+    control="IN1_R to Right Mixer Positive Resistor"
+    display_ctrl "$control"
+    CTRL_IN1_R="$CTRL_VAL"
+    printf "%s\t%s\n" "$control" "$CTRL_VAL"
 
-control="IN2_L to Left Mixer Positive Resistor"
-input_display_ctrl "$control"
-CTRL_IN2_L="$CTRL_VAL"
-printf "%s\t%s\n" "$control" $CTRL_VAL
+    control="IN2_L to Left Mixer Positive Resistor"
+    display_ctrl "$control"
+    CTRL_IN2_L="$CTRL_VAL"
+    printf "%s\t%s\n" "$control" $CTRL_VAL
 
-control="IN2_R to Right Mixer Positive Resistor"
-input_display_ctrl "$control"
-CTRL_IN2_R="$CTRL_VAL"
-printf "%s\t%s\n" "$control" $CTRL_VAL
+    control="IN2_R to Right Mixer Positive Resistor"
+    display_ctrl "$control"
+    CTRL_IN2_R="$CTRL_VAL"
+    printf "%s\t%s\n" "$control" $CTRL_VAL
+
+    alsa_ctrl='LO Playback Common Mode'
+    amixer -c $CARD get \""$alsa_ctrl"\"
+    alsa_ctrl='DAC Left Playback PowerTune'
+    amixer -c $CARD get \""$alsa_ctrl"\"
+    alsa_ctrl='DAC Right Playback PowerTune'
+    amixer -c $CARD get \""$alsa_ctrl"\"
 fi
