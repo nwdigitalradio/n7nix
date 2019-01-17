065df1f283e4fe6c5455fef4a85197e353c950bc
n7nix <basil@pacabunga.com>
2019-01-17 11:04:25 -0800

    Fix sed cmd for mail_location directory, restart dovecot service after config change.

--- email/claws/dovecot_config.sh
+++ email/claws/dovecot_config.sh
@@ -11,6 +11,13 @@ USER=
 
 function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
 
+# ===== function is_pkg_installed
+
+function is_pkg_installed() {
+
+return $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed" >/dev/null 2>&1)
+}
+
 # ===== function get_user
 
 function get_user() {
@@ -55,13 +62,25 @@ function start_service() {
             echo "Problem ENABLING $service"
         fi
     fi
-    systemctl start "$service"
-    if [ "$?" -ne 0 ] ; then
-        echo "Problem starting $service"
+    # Is service alread running?
+    systemctl is-active "$service"
+    if [ "$?" -eq 0 ] ; then
+        # service is already running, restart it to update config changes
+        systemctl restart "$service"
+        if [ "$?" -ne 0 ] ; then
+            echo "Problem re-starting $service"
+        fi
+    else
+        # service is not yet running so start it up
+        systemctl start "$service"
+        if [ "$?" -ne 0 ] ; then
+            echo "Problem starting $service"
+        fi
     fi
 }
 
 # ===== main
+
 echo -e "\n\tConfigure dovecot\n"
 
 # Be sure we're running as root
@@ -88,6 +107,19 @@ fi
 # Verify user name
 check_user
 
+pkg_name="dovecot-core"
+is_pkg_installed $pkg_name
+if [ $? -ne 0 ] ; then
+    echo "$scriptname: Will Install $pkg_name package"
+    apt-get install -y -q $pkg_name
+fi
+
+pkg_name="dovecot-imapd"
+is_pkg_installed $pkg_name
+if [ $? -ne 0 ] ; then
+    echo "$scriptname: Will Install $pkg_name package"
+    apt-get install -y -q $pkg_name
+fi
 
 # Edit these files in /etc/dovecot: dovecot.conf
 dovecot_cfg_dir="/etc/dovecot"
@@ -108,21 +140,33 @@ dovecot_cfg_dir="/etc/dovecot/conf.d"
 dovecot_cfg_file="$dovecot_cfg_dir/10-auth.conf"
 ## disable_plaintext_auth = no
 ## auth_mechanisms = plain login
-sed -i -e "/^[#]disable_plaintext_auth =/ s/disable_plaintest_auth =.*/disable_plaintest_auth = no/" $dovecot_cfg_file
+sed -i -e "/^[#]disable_plaintext_auth =/ s/disable_plaintext_auth =.*/disable_plaintext_auth = no/" $dovecot_cfg_file
 sed -i -e "/auth_mechanisms =/ s/auth_mechanisms =.*/auth_mechanisms = plain login/" $dovecot_cfg_file
 
 dovecot_cfg_file="$dovecot_cfg_dir/10-mail.conf"
 ## mail_location = maildir:~/Mail
-## mail_privileged_group = mail
-sed -i -e "/mail_location =/ s|mail_location =.*|mail_location = maildir:~/Mail|" $dovecot_cfg_file
-sed -i -e "/auth_mechanisms =/ s/auth_mechanisms =.*/auth_mechanisms = plain login/" $dovecot_cfg_file
 
-# 10-master.conf
+# Comment out any mail_location lines
+sed -i -e "/^mail_location =/ s/^/# /" $dovecot_cfg_file
+
+#sed -i -e "/mail_location =/ s|mail_location =.*|mail_location = maildir:~/Mail|" $dovecot_cfg_file
+#sed -i -e 's|^[#]mail_location = |mail_location = maildir:~/Mail\n&|'  $dovecot_cfg_file
+#awk 'FNR==NR{ if (/mail_location =/) p=NR; next} 1; FNR==p{ print "mail_location = maildir:~/Mail\n" }' $dovecot_cfg_file $dovecot_cfg_file
+
+# Add line after last occurrence of 'mail_location = '
+sed -i -e '/mail_location = [^\n]*/,$!b;//{x;//p;g};//!H;$!d;x;s//&\nmail_location = maildir:~\/Mail/' $dovecot_cfg_file
+
+## mail_privileged_group = mail
+sed -i -e "/^[#]mail_privileged_group =/ s/^[#]mail_privileged_group =.*/mail_privileged_group = mail/" $dovecot_cfg_file
+
+dovecot_cfg_file="$dovecot_cfg_dir/10-master.conf"
 ## in service imap-login
 ##     port = 143
+sed -i -e "/#port = 143/ s/#port = 143/port = 143/" $dovecot_cfg_file
 ## inet_listener imaps
 ##     port = 993
 ##    ssl = yes
+sed -i -e "/#port = 993/ s/#port = 993/port = 993/" $dovecot_cfg_file
 
 # Confirm dovecot is running
 start_service "dovecot.service"
