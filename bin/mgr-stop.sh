735764262aaeb4686b12c6ebc7ebe7113bd1bb8c
n7nix <basil@pacabunga.com>
2019-04-13 18:06:07 -0700

    Add script to stop DRAWS manager.

---
+++ bin/mgr-stop.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Script to stop WiFi access point
+# The script disables & stops the services
+
+SERVICE_LIST=" "
+
+# ===== function stop_service
+
+function stop_service() {
+    service="$1"
+    systemctl is-enabled "$service" > /dev/null 2>&1
+    if [ $? -eq 0 ] ; then
+        echo "DISABLING $service"
+        systemctl disable "$service"
+        if [ "$?" -ne 0 ] ; then
+            echo "Problem DISABLING $service"
+        fi
+    else
+        echo "Service: $service already disabled."
+    fi
+    systemctl stop "$service"
+    if [ "$?" -ne 0 ] ; then
+        echo "Problem stopping $service"
+    fi
+}
+
+# ===== main
+
+# Be sure we're running as root
+if [[ $EUID != 0 ]] ; then
+   echo "Must be root"
+   exit 1
+fi
+
+for service in `echo ${SERVICE_LIST}` ; do
+#    echo "DEBUG: Stopping service: $service"
+    stop_service $service
+done
