36ec722657583c86a639f8d6f9e1aea27b3074e9
n7nix <basil@pacabunga.com>
2019-04-19 13:31:22 -0700

    Add ip address validater.

--- config/setax25-ipaddr.sh
+++ config/setax25-ipaddr.sh
@@ -12,6 +12,37 @@ scriptname="`basename $0`"
 
 function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
 
+# ===== function valid_ip
+
+# Copied from here:
+# http://www.linuxjournal.com/content/validating-ip-address-bash-script
+#
+# Test an IP address for validity:
+# Usage:
+#      valid_ip IP_ADDRESS
+#      if [[ $? -eq 0 ]]; then echo good; else echo bad; fi
+#   OR
+#      if valid_ip IP_ADDRESS; then echo good; else echo bad; fi
+#
+function valid_ip() {
+    local  ip=$1
+    local  stat=1
+
+    dbgecho "Verifying ip address: $ip"
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    dbgecho "Verifying ip address ret: $stat"
+    return $stat
+}
+
 # ===== function get_ipaddr
 
 function get_ipaddr() {
