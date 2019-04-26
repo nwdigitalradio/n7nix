b1455684152af76d0c1c22bdb00e067b1b2af0ec
n7nix <basil@pacabunga.com>
2019-04-25 22:27:28 -0700

    Use variable for cpuinfo file name.

--- bin/piver.sh
+++ bin/piver.sh
@@ -51,7 +51,7 @@ a020d3)
 *)
    echo "Unknown pi version: $piver"
    echo "Model: $(tr -d '\0' </proc/device-tree/model)"
-   grep "Revision" /proc/cpuinfo
+   grep "Revision" $CPUINFO_FILE
 ;;
 esac
 
