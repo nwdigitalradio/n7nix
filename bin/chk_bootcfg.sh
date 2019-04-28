3aa59f8e629e112bf9e2d79a64f1ea1a42751f82
n7nix <basil@pacabunga.com>
2019-04-28 10:48:54 -0700

    Add script to verify correct overlay file & position of enable statement for BCM 2835 on-board sound driver.

---
+++ bin/chk_bootcfg.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+#
+# Check /boot/config.txt file is set up properly
+#  - verify on-board audio enable is on last line
+#  - verify proper overlay is being loaded for HAT
+
+DEBUG=1
+UPDATE_ENABLE=true
+
+DIREWOLF_CFGFILE="/etc/direwolf.conf"
+BOOT_CFGFILE="/boot/config.txt"
+
+function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
+
+# ===== function get product id of HAT
+
+# Set PROD_ID:
+# 0 = no EEPROM or no device tree found
+# 1 = HAT found but not a UDRC
+# 2 = UDRC
+# 3 = UDRC II
+# 4 = DRAWS
+# 5 = 1WSpot
+
+function get_prod_id() {
+# Initialize product ID
+PROD_ID=
+prgram="udrcver.sh"
+which $prgram
+if [ "$?" -eq 0 ] ; then
+   dbgecho "Found $prgram in path"
+   $prgram -
+   PROD_ID=$?
+else
+   currentdir=$(pwd)
+   # Get path one level down
+   pathdn1=$( echo ${currentdir%/*})
+   dbgecho "Test pwd: $currentdir, path: $pathdn1"
+   if [ -e "$pathdn1/bin/$prgram" ] ; then
+       dbgecho "Found $prgram here: $pathdn1/bin"
+       $pathdn1/bin/$prgram -1
+       PROD_ID=$?
+   else
+       echo "Could not locate $prgram default product ID to draws"
+       PROD_ID=4
+   fi
+fi
+}
+
+# ===== check onboard audio device enabled
+function chk_onboard_audio() {
+
+    # Is write turned on?
+    b_fixit=$1
+
+    # Is last line 'dtparam=audio=on' ?
+    last_line=$(tail -n1 $BOOT_CFGFILE)
+    audio_on_line="dtparam=audio=on"
+
+    if [ "$last_line" != "$audio_on_line" ] ; then
+        echo "  Last line in $BOOT_CFGFILE NOT: $audio_on_line"
+
+        # Check if update is enabled
+        if $b_fixit ; then
+            # Add Comment character to beginning of current audio line
+            sudo sed -i -e 's/^dtparam=audio=on/#&/' $BOOT_CFGFILE
+            # Add audio enable line to bottom of file
+#            sudo cat << EOT >> $BOOT_CFGFILE
+            sudo tee -a $BOOT_CFGFILE << EOT
+
+# Enable audio (loads snd_bcm2835)
+dtparam=audio=on
+EOT
+        fi
+    else
+        echo "  Last line in $BOOT_CFGFILE OK"
+    fi
+}
+
+# ===== check that dtoverlay is for correct HAT
+function chk_dtoverlay() {
+
+    # Is write turned on?
+    b_fixit=$1
+
+    set_dtoverly=
+
+    dtoverlay_name=$(grep -i "dtoverlay" $BOOT_CFGFILE | tail -n 1 | cut -d"=" -f2)
+    if [ $? -eq 0 ] ; then
+        echo "  dtoverlay currently set to: $dtoverlay_name"
+    fi
+
+    get_prod_id
+    # Test product ID for UDRC or UDRC II
+    if [[ "$PROD_ID" -eq 2 ]] || [[ "$PROD_ID" -eq 3 ]] ; then
+        # Verify dtoverlay=udrc
+        grep -i "dtoverlay=udrc" $BOOT_CFGFILE > /dev/null 2>&1
+        if [ $? -eq 0 ] ; then
+            echo "  dtoverlay for UDRC II ok"
+        else
+            echo "  Using wrong overlay for UDRC or UDRC II"
+            echo "  $(tput setaf 4)Changing from $dtoverlay_name to udrc$(tput setaf 7)"
+            if $b_fixit ; then
+#                sed 's/\(.*\)dtoverlay=/\1dtoverlay=udrc/' $BOOT_CFGFILE
+                sudo sed -ier ':a;$!{N;ba};s/^\(.*\)dtoverlay=.*/\1dtoverlay=udrc/' $BOOT_CFGFILE
+            fi
+        fi
+    elif [ "$PROD_ID" -eq 4 ] ; then
+        grep -i "dtoverlay=draws" $BOOT_CFGFILE
+        if [ $? -eq 0 ] ; then
+            echo "  dtoverlay for DRAWS ok"
+        else
+            echo "  Using wrong overlay for DRAWS HAT"
+            echo "  $(tput setaf 4)Changing from $dtoverlay_name to draws$(tput setaf 7)"
+            if $b_fixit ; then
+#                sed 's/\(.*\)^dtoverlay=.*/\1dtoverlay=draws/' $BOOT_CFGFILE
+#                 sed -ier ':a;$!{N;ba};s/^(.*\n?)dtoverlay=.*/\1dtoverlay=draws/' $BOOT_CFGFILE
+#                sudo sed -ier ':a;$!{N;ba};s/^\(.*\n?\)dtoverlay=.*/\1dtoverlay=draws/' $BOOT_CFGFILE
+                 sudo sed -ier ':a;$!{N;ba};s/^\(.*\)dtoverlay=.*/\1dtoverlay=draws/' /boot/config.txt
+            fi
+        fi
+    fi
+}
+
+# ===== main
+
+echo
+echo "Verify IF dtoverlay= is set properly"
+chk_dtoverlay $UPDATE_ENABLE
+echo
+echo "Verify IF dtparam=audio is set properly"
+chk_onboard_audio $UPDATE_ENABLE
+
