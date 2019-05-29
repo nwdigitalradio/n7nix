3dfe043e00c95fb9cb11d53535baf7a050fdc231
n7nix <basil@pacabunga.com>
2019-05-29 15:49:41 -0700

    Add toggle option, to toggle PTT state

--- debug/ptt_ctrl.sh
+++ debug/ptt_ctrl.sh
@@ -22,10 +22,12 @@
 
 scriptname="`basename $0`"
 
-# default to read gpio
+# default to READ gpio
 b_gpioWRITE=false
-# default to turn gpio off
+# default to turn gpio OFF
 b_gpioON=false
+# default to NOT toggle gpio state
+b_gpioTOGGLE=false
 
 function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
 
@@ -37,6 +39,7 @@ function usage() {
    echo "   -r | --right   Select right connector"
    echo "   -on            Turn selected gpio ON"
    echo "   -off           Turn selected gpio OFF"
+   echo "   -t | --toggle  Toggle PTT state"
    echo "   -c | --check   Check gpio state ie. read gpio"
    echo "   -d | --debug   Turn debug on"
    echo "   -h             display this message"
@@ -66,6 +69,9 @@ while [[ $# -gt 0 ]] ; do
          b_gpioON=false
          b_gpioWRITE=true
          ;;
+      -t |--toggle)
+         b_gpioTOGGLE=true
+         ;;
       -d|--debug)
          DEBUG=1
          dbgecho "Debug flag set"
@@ -91,6 +97,19 @@ dbgecho "Options: set gpio: $gpio_num, state: On=$b_gpioON, action: Write=$b_gpi
 # Set gpio mode to output
 gpio -g mode $gpio_num out
 
+if $b_gpioTOGGLE ; then
+    # Get current state
+    gpio_state=$(gpio -g read $gpio_num)
+    dbgecho "gpio state=$gpio_state"
+    if [ "$gpio_state" = 0 ] ; then
+        dbgecho "Turn PTT on gpio: $gpio_num"
+        gpio -g write $gpio_num 1
+    else
+        dbgecho "Turn PTT off gpio: $gpio_num"
+        gpio -g write $gpio_num 0
+    fi
+    exit
+fi
 if $b_gpioWRITE ; then
     if $b_gpioON ; then
         gpio -g write $gpio_num 1
