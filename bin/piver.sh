49efdc912945dccb9151efe2a38625b7da6d8040
n7nix <basil@pacabunga.com>
2019-06-27 15:21:36 -0700

    Update RPi revision numbers for Pi 4B

--- bin/piver.sh
+++ bin/piver.sh
@@ -12,40 +12,56 @@ piver="$(grep "Revision" $CPUINFO_FILE)"
 piver="$(echo -e "${piver##*:}" | tr -d '[[:space:]]')"
 
 case $piver in
+9020e0)
+   echo " Pi 3 Model A+, Rev 1.0, Mfg by Sony UK"
+   HAS_WIFI=true
+;;
 a01040)
-   echo " Pi 2 Model B Mfg by Sony UK"
+   echo " Pi 2 Model B, Rev 1.0, Mfg by Sony UK"
 ;;
 a01041)
-   echo " Pi 2 Model B Mfg by Sony UK"
-;;
-a21041)
-   echo " Pi 2 Model B Mfg by Embest"
-;;
-a22042)
-   echo " Pi 2 Model B with BCM2837 Mfg by Embest"
+   echo " Pi 2 Model B, Rev 1.1, Mfg by Sony UK"
 ;;
 a02082)
-   echo " Pi 3 Model B Mfg by Sony UK"
-   HAS_WIFI=true
-;;
-a22082)
-   echo " Pi 3 Model B Mfg by Embest"
-   HAS_WIFI=true
-;;
-a32082)
-   echo " Pi 3 Model B Mfg by Sony Japan"
-   HAS_WIFI=true
-;;
-a52082)
-   echo " Pi 3 Model B Mfg by Stadium"
+   echo " Pi 3 Model B, Rev 1.2, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 a020d3)
-   echo " Pi 3 Model B+ Mfg by Sony UK"
+   echo " Pi 3 Model B+,  Rev 1.3, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
-9020e0)
-   echo " Pi 3 Model A+ Mfg by Sony UK"
+a21041)
+   echo " Pi 2 Model B, Rev 1.1, Mfg by Embest"
+;;
+a22042)
+   echo " Pi 2 Model B with BCM2837, Rev 1.2, Mfg by Embest"
+;;
+a22082)
+   echo " Pi 3 Model B, Rev 1.2, Mfg by Embest"
+   HAS_WIFI=true
+;;
+a32082)
+   echo " Pi 3 Model B, Rev 1.2, Mfg by Sony Japan"
+   HAS_WIFI=true
+;;
+a52082)
+   echo " Pi 3 Model B, Rev 1.2, Mfg by Stadium"
+   HAS_WIFI=true
+;;
+a22083)
+   echo " Pi 3 Model B, Rev 1.3, Mfg by Embest"
+   HAS_WIFI=true
+;;
+a03111)
+   echo " Pi 4 Model B,  Rev 1.1, 1GB mem, Mfg by Sony UK"
+   HAS_WIFI=true
+;;
+b03111)
+   echo " Pi 4 Model B,  Rev 1.1, 2GB mem, Mfg by Sony UK"
+   HAS_WIFI=true
+;;
+c03111)
+   echo " Pi 4 Model B,  Rev 1.1, 4GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 *)
