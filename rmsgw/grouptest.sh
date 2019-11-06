99f66fd495cfdbe808a1ff151b543ded7faf866b
n7nix <basil@pacabunga.com>
2019-11-06 13:29:10 -0800

    Fix permissions on /usr/local/etc/rmgw directory.

---
+++ rmsgw/grouptest.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+#
+# Does rmsgw group exist?
+GROUP="rmsgw"
+
+# ===== main
+
+# must be root
+if [[ $EUID != 0 ]] ; then
+   echo "Run as root"
+   exit 1
+fi
+
+
+if [ $(getent group $GROUP) ]; then
+    echo "group $GROUP exists."
+else
+    echo "group $GROUP does not exist...adding"
+     /usr/sbin/groupadd $GROUP
+    usermod -a -G rmsgw rmsgw
+fi
+
+# This also works
+#grep -q -E "^$GROUP:" /etc/group
+#if [ $? -ne 0 ] ; then
+    ## rmsgw group does not exist
+    #group add rmsgw
+    #usermod -a -G rmsgw rmsgw
+#fi
+
+DIR=" /usr/local/etc/rmsgw"
+echo "Change owner:group of directory: $DIR"
+chown -R rmsgw:rmsgw $DIR
