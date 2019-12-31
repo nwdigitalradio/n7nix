bb8353df29377a3ed24d6fbb4d93ee403c143ac1
n7nix <basil@pacabunga.com>
2019-12-30 17:21:25 -0800

    Parameterize some package variables & make work with new checkinstall.

--- ax25/mkdeb.sh
+++ ax25/mkdeb.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 #
 # Build debian packages for libax25, ax25apps, ax25tools
-user="gunn"
 scriptname="`basename $0`"
+user="pi"
+maintainer="gunn@beeble"
 
-SRC_DIR="/usr/local/src/ax25/linuxax25-master"
+#SRC_DIR="/usr/local/src/ax25/linuxax25-master"
+SRC_DIR="/usr/local/src/linuxax25"
 PKGDIR="/home/$user/n7nix/ax25/debpkg/"
 
-pkgname="libax25"
-SUFFIX="1.1.1-1_armhf.deb"
-PKGLONGNAME="$pkgname"_"$SUFFIX"
+# ===== main
 
 # Be sure we are running as root
 if (( `id -u` != 0 )); then
    echo "$scriptname: Sorry, must be root.  Exiting...";
    exit 1;
 fi
+
+prog_name="checkinstall"
+type -P $prog_name  >/dev/null 2>&1
+if [ "$?"  -ne 0 ]; then
+    # checkinstall not installed
+    apt-get install -y -q $prog_name
+fi
+
+PROGRAM_LIST="ax25apps ax25tools libax25"
+for prog_name in $PROGRAM_LIST ; do
+    vernum=$(grep -i AC_INIT $SRC_DIR/$prog_name/configure.ac)
+    vernum=$(echo "$vernum" | cut -f2 -d' '|cut -f1 -d',')
+    echo "Updating $prog_name to version: $vernum"
+done
+
+pkgname="libax25"
+pkgver="1.1.3"
+SUFFIX="$pkgver-1_armhf.deb"
+PKGLONGNAME="$pkgname"_"$SUFFIX"
 
 echo
 echo " ===== Make $pkgname Debian package"
@@ -27,8 +46,9 @@ pushd $SRC_DIR/$pkgname
 checkinstall -D --nodoc \
 --pkglicense=GPL2 \
 --requires="" \
---maintainer="gunn@brox" \
+--maintainer="$maintainer" \
 --pkgname=$pkgname \
+--pkgversion="$pkgver"\
 --pakdir=$PKGDIR \
 make install
 
@@ -42,7 +62,8 @@ fi
 # Summary: AX.25 ham radio applications
 
 pkgname="ax25apps"
-SUFFIX="2.0.0-1_armhf.deb"
+pkgver="2.0.1"
+SUFFIX="$pkgver-1_armhf.deb"
 PKGLONGNAME="$pkgname"_"$SUFFIX"
 
 echo
@@ -55,8 +76,9 @@ pushd $SRC_DIR/$pkgname
 checkinstall -D --nodoc \
 --pkglicense=GPL2 \
 --requires="libax25 \(\>= 1.0.0\)" \
---maintainer="gunn@brox" \
+--maintainer="$maintainer" \
 --pkgname=$pkgname \
+--pkgversion="$pkgver" \
 --pakdir=$PKGDIR \
 make install installconf
 
@@ -70,7 +92,8 @@ fi
 # Summary:  tools for AX.25 interface configurqation
 
 pkgname="ax25tools"
-SUFFIX="1.0.4-1_armhf.deb"
+pkgver="1.0.5"
+SUFFIX="$pkgver-1_armhf.deb"
 PKGLONGNAME="$pkgname"_"$SUFFIX"
 
 echo
@@ -83,8 +106,9 @@ pushd $SRC_DIR/$pkgname
 checkinstall -D --nodoc \
 --pkglicense=GPL2 \
 --requires="libax25 \(\>= 1.0.0\)" \
---maintainer="gunn@brox" \
+--maintainer="$maintainer" \
 --pkgname=$pkgname \
+--pkgversion="$pkgver" \
 --pakdir=$PKGDIR \
 make install installconf
 
