8ac0f6444a08124c9cbcede70f8d7f7ca90cde70
n7nix <basil@pacabunga.com>
2020-01-01 09:34:11 -0800

    Make display of installed ax.25 packages a function.

--- ax25/ax_verchk.sh
+++ ax25/ax_verchk.sh
@@ -29,11 +29,23 @@ bupgrade=false
 function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
 
 # ===== function version_gt
+
 # Determine if first argument $1 is greater than second argument
 
 function version_gt() {
 #    printf 'version_gt: %s\n' "$@" | sort -V | head -n 1
     test "$(printf '%s\n' "$@" | sort --version-sort | head -n 1)" != "$1";
+}
+
+# ===== function display_ax25pkgver
+# Display installed AX.25 package versions
+
+function display_ax25pkgver() {
+    install[lib]=$(dpkg -l "libax25"  | tail -n 1 | tr -s '[[:space:]]' | cut -f3 -d' ' | cut -f1 -d'-')
+    install[app]=$(dpkg -l "ax25apps" | tail -n 1 | tr -s '[[:space:]]' | cut -f3 -d' ' | cut -f1 -d'-')
+    install[tool]=$(dpkg -l "ax25tools" | tail -n 1 | tr -s '[[:space:]]' | cut -f3 -d' ' | cut -f1 -d'-')
+
+    echo "install:  lib: ${install[lib]}, app: ${install[app]}, tool: ${install[tool]}"
 }
 
 # ===== main
@@ -52,22 +64,19 @@ fetrepo[tool]=$(echo $fetrepo_toolver | cut -f2 -d ' ' | cut -f1 -d',')
 echo "fet repo: lib: ${fetrepo[lib]}, app: ${fetrepo[app]}, tool: ${fetrepo[tool]}"
 
 
-nixrepo[lib]=$(ls -1 /home/pi/n7nix/ax25/debpkg/*.deb | grep -i libax25 | cut -f2 -d'_' | cut -f1 -d'-')
-nixrepo[app]=$(ls -1 /home/pi/n7nix/ax25/debpkg/*.deb | grep -i ax25apps | cut -f2 -d'_' | cut -f1 -d'-')
-nixrepo[tool]=$(ls -1 /home/pi/n7nix/ax25/debpkg/*.deb | grep -i ax25tools | cut -f2 -d'_' | cut -f1 -d'-')
+nixrepo[lib]=$(ls -1 $HOME/n7nix/ax25/debpkg/*.deb | grep -i libax25 | cut -f2 -d'_' | cut -f1 -d'-')
+nixrepo[app]=$(ls -1 $HOME/n7nix/ax25/debpkg/*.deb | grep -i ax25apps | cut -f2 -d'_' | cut -f1 -d'-')
+nixrepo[tool]=$(ls -1 $HOME/n7nix/ax25/debpkg/*.deb | grep -i ax25tools | cut -f2 -d'_' | cut -f1 -d'-')
 
 echo "nix repo: lib: ${nixrepo[lib]}, app: ${nixrepo[app]}, tool: ${nixrepo[tool]}"
 
-install[lib]=$(dpkg -l "libax25"  | tail -n 1 | tr -s '[[:space:]]' | cut -f3 -d' ' | cut -f1 -d'-')
-install[app]=$(dpkg -l "ax25apps" | tail -n 1 | tr -s '[[:space:]]' | cut -f3 -d' ' | cut -f1 -d'-')
-install[tool]=$(dpkg -l "ax25tools" | tail -n 1 | tr -s '[[:space:]]' | cut -f3 -d' ' | cut -f1 -d'-')
-
-echo "install:  lib: ${install[lib]}, app: ${install[app]}, tool: ${install[tool]}"
+# Display installed AX.25 package versions
+display_ax25pkgver
 
 PROGLIST="lib app tool"
 
 # Check if nix repo up-to-date
-
+bnixupdate=false
 for prog in $PROGLIST ; do
     first_version="${fetrepo[$prog]}"
     second_version="${nixrepo[$prog]}"
@@ -76,12 +85,18 @@ for prog in $PROGLIST ; do
 
     if version_gt "$first_version" "$second_version" ; then
         echo "Repo version: $first_version is greater than installed version $second_version, n7nix needs to update his repo for $prog"
+        bnixupdate=true
     fi
 done
 
+if $bnixupdate ; then
+    echo "Update to n7nix repo required ... exiting."
+    exit 1
+fi
+
 
 # Check if installed version up-to-date
-
+binstallupdate=false
 for prog in $PROGLIST ; do
     first_version="${fetrepo[$prog]}"
     second_version="${install[$prog]}"
@@ -90,8 +105,25 @@ for prog in $PROGLIST ; do
 
     if version_gt "$first_version" "$second_version" ; then
         echo "$first_version is greater than $second_version, will install newer ax25 $prog"
+        binstallupdate=true
     fi
 done
+
+if $binstallupdate ; then
+
+    if $bupgrade ; then
+        echo "AX.25 packages will be updated"
+        sudo dpkg -i $HOME/n7nix/ax25/debpkg/libax25_${nixrepo[lib]}-1_armhf.deb
+        sudo dpkg -i $HOME/n7nix/ax25/debpkg/ax25apps_${nixrepo[app]}-1_armhf.deb
+        sudo dpkg -i $HOME/n7nix/ax25/debpkg/ax25tools_${nixrepo[tool]}-1_armhf.deb
+
+        # Verify installation of installed AX.25 packages
+        # Display installed AX.25 package versions
+        display_ax25pkgver
+    else
+        echo "AX.25 packages would have been updated"
+    fi
+fi
 
 
 if [ ! -z "$DEBUG" ] ; then
@@ -102,6 +134,5 @@ if [ ! -z "$DEBUG" ] ; then
 
     if version_gt $first_version $second_version; then
         echo "$first_version is greater than $second_version !"
-        bupgrade=true
     fi
 fi
