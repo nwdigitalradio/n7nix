fbabd32041dc0e9ce734f38fd5fa3c7f98e3bfe0
n7nix <basil@pacabunga.com>
2020-01-22 08:17:15 -0800

    Fix no output from commands & verify return codes.

--- ax25/ax25ipd-start.sh
+++ ax25/ax25ipd-start.sh
@@ -1,26 +1,49 @@
-#!/bin/sh
+#!/bin/bash
 #
+# DEBUG=1
+
 SBINDIR=/usr/local/sbin
 
 AX25IPD_KISSOUT_FILE="/tmp/ax25ipd-config.tmp"
-AX25IPD_DEVICE_FILE="/tmp/ax25ipd-config-tmp"
+AX25IPD_TMP_FILE="/tmp/ax25ipd-config-tmp"
+
+function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
 
 echo "Installing ax25ipd Unix98 master pseudo tty"
-# specify config file path & log level
-AXUDP=$($SBINDIR/ax25ipd -c /etc/ax25/ax25ipd.conf -l3 | tail -1)
-# Get process id of ax25ipd
-echo $! > /var/run/ax25ipd.pid
+# specify ax25ipd config file path & log level
+$SBINDIR/ax25ipd -c /etc/ax25/ax25ipd.conf -l3 > $AX25IPD_TMP_FILE
+retcode="$?"
+ax25ipd_pid=$(pidof ax25ipd)
+dbgecho "DEBUG: ax25ipd ret: $retcode, pid: $ax25ipd_pid"
+if [ "$retcode" -ne 0 ] ; then
+    echo "Failed to run ax25ipd"
+    if [ -z "$ax25ipd_pid" ] ; then
+        kill "$ax25ipd_pid"
+    fi
+    exit
+else
+    echo "ax25ipd running with pid: $ax25ipd_pid"
+    # Save process id of ax25ipd
+    echo "$ax25ipd_pid" > /var/run/ax25ipd.pid
+fi
+
+AXUDP=$(cat $AX25IPD_TMP_FILE | tail -n1)
+dbgecho "DEBUG: AXUDP: $AXUDP"
 export AXUDP=$AXUDP
-#
+
 echo "Installing a KISS link with pseudo term: $AXUDP on ethernet port"
 $SBINDIR/kissattach  $AXUDP axudp  > $AX25IPD_KISSOUT_FILE
- awk '/device/ { print $7 }' $AX25IPD_KISSOUT_FILE > $AX25IPD_DEVICE_FILE
-read Device < $AX25IPD_DEVICE_FILE
+if [ "$?" -ne 0 ] ; then
+    echo "Kissattach failed to attach pseudo term: $AXUDP"
+    exit
+fi
 
-#Check for Device
+awk '/device/ { print $7 }' $AX25IPD_KISSOUT_FILE > $AX25IPD_TMP_FILE
+read Device < $AX25IPD_TMP_FILE
+
+# Check for Device
 if [ -d /proc/sys/net/ax25/$Device ] ; then
     echo "Port axudp attached to $Device"
-    cd /proc/sys/net/ax25/$Device/
 else
     echo "Device: $Device does NOT exist"
 fi
