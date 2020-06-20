9c95a93d84340dd9689e770e8209bb5b1172ed95
n7nix <basil@pacabunga.com>
2020-06-20 10:54:22 -0700

    Change build essential package from ncurses-dev to libncurses-dev add error_exit function.

--- ardop/ardop_install.sh
+++ ardop/ardop_install.sh
@@ -14,30 +14,44 @@
 # DEBUG=1
 
 force_update=false
-scriptname="`basename $0`"
-user=$(whoami)
+scriptname="$(basename "$0")"
+USER=
+
 UDR_INSTALL_LOGFILE="/var/log/udr_install.log"
 
 SRC_DIR="/usr/local/src/"
-BUILD_PKG_REQUIRE="build-essential ncurses-dev zlib1g-dev"
+BUILD_PKG_REQUIRE="build-essential libncurses-dev zlib1g-dev"
 
 function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
+
+# ===== function error_exit
+# Display string argument & exit
+
+function error_exit() {
+	echo "$(tput setaf 1)$1$(tput setaf 7)" 1>&2
+	exit 1
+}
 
 # ===== function is_pkg_installed
 
 function is_pkg_installed() {
 
-return $(dpkg-query -W -f='${Status}' $1 2>/dev/null | grep -c "ok installed" >/dev/null 2>&1)
+    if [ -z "$1" ] ; then
+        error_exit "is_pkg_installed: no argument found"
+    fi
+    retval="$(dpkg-query -W -f='${Status}' "$1" 2>/dev/null | grep -c "ok installed")"
+#    echo "is_pkg_installed debug: arg: $1, retval: $retval"
+    return "$retval"
 }
 
 # ===== function get_user
 
 function get_user() {
    # Check if there is only a single user on this system
-   if (( `ls /home | wc -l` == 1 )) ; then
+   if (( $(ls /home | wc -l) == 1 )) ; then
       USER=$(ls /home)
    else
-      echo "Enter user name ($(echo $USERLIST | tr '\n' ' ')), followed by [enter]:"
+      echo "Enter user name ($(echo "$USERLIST" | tr '\n' ' ')), followed by [enter]:"
       read -e USER
    fi
 }
@@ -67,7 +81,7 @@ function get_user_name() {
 
     # Get list of users with home directories
     USERLIST="$(ls /home)"
-    USERLIST="$(echo $USERLIST | tr '\n' ' ')"
+    USERLIST="$(echo "$USERLIST" | tr '\n' ' ')"
 
     # Check if user name was supplied on command line
     if [ -z "$USER" ] ; then
@@ -84,26 +98,27 @@ function wiseman_download() {
 if [ ! -e "$download_filename" ] || $force_update ; then
 
     if [ -e "$download_filename" ] ; then
-        echo "Deleting $(stat -c "%y %s %n" $download_filename)"
-        rm $download_filename
+        echo "Deleting $(stat -c "%y %s %n" "$download_filename")"
+        rm "$download_filename"
     fi
 
-    wget http://www.cantab.net/users/john.wiseman/Downloads/Beta/$download_filename
+    wget http://www.cantab.net/users/john.wiseman/Downloads/Beta/"$download_filename"
     if [ $? -ne 0 ] ; then
         echo -e "\n$(tput setaf 1)FAILED to download file: $download_filename$(tput setaf 7)\n"
     else
         echo -e "\n$(tput setaf 1)Successfully downloaded: $download_filename $(tput setaf 7)\n"
         # Make executable
-        chmod +x $download_filename
+        chmod +x "$download_filename"
     fi
 else
-    val=$(stat -c "%y %s %n" $download_filename)
+    val=$(stat -c "%y %s %n" "$download_filename")
 
-    val2=$(echo $val | tr ' ' '\n' | tail -2 | xargs -n2)
+    val2=$(echo "$val" | tr ' ' '\n' | tail -2 | xargs -n2)
     val=${val%.*}
     echo "Using existing file: $val $val2"
 fi
 }
+
 
 # ===== main
 
@@ -114,27 +129,36 @@ if (( $# != 0 )) ; then
    USER="$1"
 fi
 
-get_user_name
+# running as root?
+if [[ $EUID != 0 ]] ; then
+    if [ -z "$USER" ] ; then
+        USER=$(whoami)
+    fi
+else
+    get_user_name
+fi
 
 BIN_DIR="/home/$USER/bin"
-cd $BIN_DIR
+cd $BIN_DIR || error_exit "Can not change directory to: $BIN_DIR"
 
 # Note:
 #   piardopc is version 1 of the ardop TNC
 #   piardop2 is version 2 of the ardop TNC
 
 PROGLIST="piARDOP_GUI piardop2 piardopc"
-for prog_name in `echo ${PROGLIST}` ; do
+# echo "DEBUG: for progname in ${PROGLIST}"
+for prog_name in ${PROGLIST} ; do
+#    echo  "DEBUG: using: $prog_name"
     download_filename="$prog_name"
     wiseman_download
 done
 
 # Set up virtual sound device ARDOP
 mod_file="/home/$USER/.asoundrc"
-grep -i "pcm.ARDOP" $mod_file > /dev/null 2>&1
+grep -i "pcm.ARDOP" "$mod_file" > /dev/null 2>&1
 if [ $? -ne 0 ] ; then
     # Add to bottom of file
-    cat << EOT >> $mod_file
+    cat << EOT >> "$mod_file"
 
 pcm.ARDOP {
         type rate
@@ -152,10 +176,11 @@ fi
 dbgecho "Check build packages: $BUILD_PKG_REQUIRE"
 needs_pkg=false
 
-for pkg_name in `echo ${BUILD_PKG_REQUIRE}` ; do
-
-   is_pkg_installed $pkg_name
-   if [ $? -ne 0 ] ; then
+#echo "DEBUG: for pkg_name in ${BUILD_PKG_REQUIRE}"
+for pkg_name in ${BUILD_PKG_REQUIRE} ; do
+#    echo  "DEBUG: using: $pkg_name"
+   is_pkg_installed "$pkg_name"
+   if [ $? -eq 0 ] ; then
       echo "$scriptname: Will Install $pkg_name package"
       needs_pkg=true
       break
@@ -184,8 +209,8 @@ ARIM_SRC_DIR=$SRC_DIR/arim-$arim_ver
 # Should check if there is a previous installation
 
 if [ ! -d "$ARIM_SRC_DIR" ] ; then
-    cd "$SRC_DIR"
-    sudo chown $USER:$USER .
+    cd "$SRC_DIR" || error_exit "Can not change directory to: $SRC_DIR"
+    sudo chown "$USER":"$USER" .
 
     sudo wget https://www.whitemesa.net/arim/src/$download_filename
         if [ $? -ne 0 ] ; then
@@ -193,10 +218,10 @@ if [ ! -d "$ARIM_SRC_DIR" ] ; then
         else
             sudo tar xzvf $download_filename
             if [ $? -ne 0 ] ; then
-                echo "$(tput setaf 1)FAILED to untar file: $download_filname $(tput setaf 7)"
+                echo "$(tput setaf 1)FAILED to untar file: $download_filename $(tput setaf 7)"
             else
-                sudo chown -R $USER:$USER $ARIM_SRC_DIR
-                cd $ARIM_SRC_DIR
+                sudo chown -R "$USER":"$USER" "$ARIM_SRC_DIR"
+                cd "$ARIM_SRC_DIR" || error_exit "Can not change directory to: $ARIM_SRC_DIR"
                 ./configure
                 echo -e "\n$(tput setaf 4)Starting arim build $(tput setaf 7)\n"
                 make
@@ -208,7 +233,6 @@ else
     echo -e "Using previously built arim-$arim_ver\n"
     echo
 fi
-
 
 echo
 echo "$(date "+%Y %m %d %T %Z"): $scriptname: arim & ardop install script FINISHED" | sudo tee -a $UDR_INSTALL_LOGFILE
