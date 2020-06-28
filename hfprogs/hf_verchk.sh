e5f7e79411b642ca767614db7c27a0b147d06b9c
n7nix <basil@pacabunga.com>
2020-06-28 10:50:03 -0700

    Reference correct version number.

--- hfprogs/hf_verchk.sh
+++ hfprogs/hf_verchk.sh
@@ -361,7 +361,7 @@ if [ -z "$DEBUG1" ] ; then
     # wsjtx_ver=$(curl -s $ver_url | grep -A 1 -i "Availability (GA)" | tail -n 1 | cut -d '<' -f1)
     #           $(curl -s $ver_url | grep -i "Availability (GA)" | cut -d '>' -f3 | cut -d '<' -f1)
 
-    # This removes all html tags:  sed -e 's/<[^>]*>//g' worked for 2.2.1 but not 2.2.2
+    # This removes all html tags:  sed -e 's/<[^>]*>//g' worked for 2.1.2 but not 2.2.2
 #    wsjtx_ver=$(curl -s $ver_url | grep -A 1 -i "Availability (GA).*" | head -n 1 | sed -e 's/<[^>]*>//g' | sed -n 's/.*WSJT-X//p')
      # Works for 2.2.2
      wsjtx_ver=$(curl -s http://physics.princeton.edu/pulsar/K1JT/wsjtx.html | grep -A 1 -i "Availability (GA).*" | tail -n 1 |  sed -e 's/<[^>]*>//g')
