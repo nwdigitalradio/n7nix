f624be1b4e603b13d2b8004538e9043c0a14eaac
n7nix <basil@pacabunga.com>
2020-09-03 09:16:47 -0700

    Add a short version string.

--- bin/piver.sh
+++ bin/piver.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 #
-# Raspberry Pi version check based on Revision number from cpuinfo
+# Display Raspberry Pi model & revision number from /proc/cpuinfo
+# If any command line arguments detected the short version is displayed
+#
+# Hardware History
+# https://elinux.org/RPi_HardwareHistory
+#
+# RPi latest Revision Codes
+# https://www.raspberrypi.org/documentation/hardware/raspberrypi/revision-codes/README.md
+#
 
 CPUINFO_FILE="/proc/cpuinfo"
 HAS_WIFI=false
+QUIET_MODE=false
+
+# Check if there are any args on command line
+if (( $# != 0 )) ; then
+    QUIET_MODE=true
+fi
 
 # This method works as well
 #piver="$(grep "Revision" $CPUINFO_FILE | cut -d':' -f2- | tr -d '[[:space:]]')"
@@ -13,79 +27,88 @@ piver="$(echo -e "${piver##*:}" | tr -d '[[:space:]]')"
 
 case $piver in
 9020e0)
-   echo " Pi 3 Model A+, Rev 1.0, Mfg by Sony UK"
+   VERSION_STRING=" Pi 3 Model A+, Rev 1.0, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 a01040)
-   echo " Pi 2 Model B, Rev 1.0, Mfg by Sony UK"
+   VERSION_STRING=" Pi 2 Model B, Rev 1.0, Mfg by Sony UK"
 ;;
 a01041)
-   echo " Pi 2 Model B, Rev 1.1, Mfg by Sony UK"
+   VERSION_STRING=" Pi 2 Model B, Rev 1.1, Mfg by Sony UK"
 ;;
 a02082)
-   echo " Pi 3 Model B, Rev 1.2, Mfg by Sony UK"
+   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 a020d3)
-   echo " Pi 3 Model B+, Rev 1.3, Mfg by Sony UK"
+   VERSION_STRING=" Pi 3 Model B+, Rev 1.3, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 a21041)
-   echo " Pi 2 Model B, Rev 1.1, Mfg by Embest"
+   VERSION_STRING=" Pi 2 Model B, Rev 1.1, Mfg by Embest"
 ;;
 a22042)
-   echo " Pi 2 Model B with BCM2837, Rev 1.2, Mfg by Embest"
+   VERSION_STRING=" Pi 2 Model B with BCM2837, Rev 1.2, Mfg by Embest"
 ;;
 a22082)
-   echo " Pi 3 Model B, Rev 1.2, Mfg by Embest"
+   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Embest"
    HAS_WIFI=true
 ;;
 a32082)
-   echo " Pi 3 Model B, Rev 1.2, Mfg by Sony Japan"
+   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Sony Japan"
    HAS_WIFI=true
 ;;
 a52082)
-   echo " Pi 3 Model B, Rev 1.2, Mfg by Stadium"
+   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Stadium"
    HAS_WIFI=true
 ;;
 a22083)
-   echo " Pi 3 Model B, Rev 1.3, Mfg by Embest"
+   VERSION_STRING=" Pi 3 Model B, Rev 1.3, Mfg by Embest"
    HAS_WIFI=true
 ;;
 a03111)
-   echo " Pi 4 Model B, Rev 1.1, 1GB mem, Mfg by Sony UK"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.1, 1GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 b03111)
-   echo " Pi 4 Model B, Rev 1.1, 2GB mem, Mfg by Sony UK"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.1, 2GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 b03112)
-   echo " Pi 4 Model B, Rev 1.2, 2GB mem, Mfg by Sony UK"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.2, 2GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 c03111)
-   echo " Pi 4 Model B, Rev 1.1, 4GB mem, Mfg by Sony UK"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.1, 4GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 c03112)
-   echo " Pi 4 Model B, Rev 1.2, 4GB mem, Mfg by Sony UK"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.2, 4GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 d03114)
-   echo " Pi 4 Model B, Rev 1.4, 8GB mem, Mfg by Sony UK"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.4, 8GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 *)
    echo "Unknown pi version: $piver"
    echo "Model: $(tr -d '\0' </proc/device-tree/model)"
    grep "Revision" $CPUINFO_FILE
+   exit 1
 ;;
 esac
 
 retcode=1
-if $HAS_WIFI ; then
-   echo " Has WiFi"
-   retcode=0
+if $QUIET_MODE ; then
+    SHORT_STRING=$(echo "$VERSION_STRING" | cut -f1 -d',')
+    echo "$SHORT_STRING"
+else
+    WIFI_STRING=
+    if $HAS_WIFI ; then
+        WIFI_STRING="with WiFi"
+        retcode=0
+    fi
+
+    echo "$VERSION_STRING $WIFI_STRING"
 fi
 exit $retcode
