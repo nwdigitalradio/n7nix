424e6da7b849b30288f16107773b66a63ae118fe
n7nix <basil@pacabunga.com>
2020-11-01 10:46:24 -0800

    Add quiet console output option.

--- ax25/ax25-reset.sh
+++ ax25/ax25-reset.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 USER=$(whoami)
+QUIET=
 
-sudo /home/$USER/bin/ax25-stop
+# Check if there are any args on command line
+# Use to quiet output
+if (( $# != 0 )) ; then
+   QUIET="-q"
+fi
+
+sudo /home/$USER/bin/ax25-stop $QUIET
 sleep 1
-sudo /home/$USER/bin/ax25-start
+sudo /home/$USER/bin/ax25-start $QUIET
