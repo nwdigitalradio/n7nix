a8f4d1960f0bb83d0a56f8bd4832e7225806ef17
n7nix <basil@pacabunga.com>
2020-11-05 11:35:27 -0800

    Add display_service_status.

--- watchdog/wd-status.sh
+++ watchdog/wd-status.sh
@@ -2,11 +2,32 @@
 #
 #  wd_status.sh
 
+# ===== function display_service_status
+function display_service_status() {
+    service="$1"
+    if systemctl is-enabled --quiet "$service" ; then
+        enabled_str="enabled"
+    else
+        enabled_str="NOT enabled"
+    fi
+
+    if systemctl is-active --quiet "$service" ; then
+        active_str="running"
+    else
+        active_str="NOT running"
+    fi
+    echo "Service: $service is $enabled_str and $active_str"
+}
+
+
+# ===== main
+
+display_service_status watchdog
 RSTS_REG=$(vcgencmd get_rsts)
 
 REBOOT_CNT=$(last reboot | grep ^reboot | wc -l)
 
-echo "RSTS: $RSTS_REG, reboot count: $REBOOT_CNT"
+echo "=== RSTS: $RSTS_REG, reboot count: $REBOOT_CNT"
 
-echo "SYS log"
+echo "=== SYS log"
 grep -i watchdog /var/log/syslog
