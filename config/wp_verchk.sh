666a70b8b408b14477433288401cfebd22045884
n7nix <basil@pacabunga.com>
2020-12-01 12:47:44 -0800

    Get WiringPi current source version # from github source

--- config/wp_verchk.sh
+++ config/wp_verchk.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 #
 # Get latest version of WiringPi
-CURRENT_VER="2.60"
+WP_GITHUB_VER=
+CURRENT_WP_VER="2.60"
 SRCDIR=/usr/local/src
+GITHUB_VER_FILE="/usr/local/src/WiringPi/VERSION"
+
+
+# ===== function get_wp_gitver
+# Get current version  WiringPi
+function get_wp_gitver() {
+    pushd $SRCDIR > /dev/null
+    if [ -e $SRCDIR/WiringPi ] ; then
+        cd WiringPi
+	git pull
+    else
+        git clone https://github.com/WiringPi/WiringPi
+    fi
+    popd > /dev/null
+
+    if [ -e  $GITHUB_VER_FILE ] ; then
+        WP_GITHUB_VER=$(cat $GITHUB_VER_FILE)
+    fi
+}
 
 # ===== function get_wp_ver
-# Get current version of WiringPi
+# Get current version of installed WiringPi
 function get_wp_ver() {
     wp_ver=$(gpio -v | grep -i "version" | cut -d':' -f2)
 
@@ -19,8 +39,15 @@ function get_wp_ver() {
 
 # ===== main
 get_wp_ver
-echo "WiringPi current version: $wp_ver"
-if [ "$wp_ver" != "$CURRENT_VER" ] ; then
+get_wp_gitver
+echo "WiringPi current version: $wp_ver, github version: $WP_GITHUB_VER"
+
+LATEST_WP_VER=$CURRENT_WP_VER
+if [ ! -z $WP_GITHUB_VER ] ; then
+    LATEST_WP_VER=$WP_GITHUB_VER
+fi
+
+if [ "$wp_ver" != "$LATEST_WP_VER" ] ; then
     echo "Installing latest version of WiringPi"
     # Setup tmp directory
     if [ ! -d "$SRCDIR" ] ; then
@@ -32,8 +59,9 @@ if [ "$wp_ver" != "$CURRENT_VER" ] ; then
     # sudo dpkg -i wiringpi-latest.deb
 
     # Build WiringPi from source
-    pushd $SRCDIR
-    git clone https://github.com/WiringPi/WiringPi
+    pushd $SRCDIR > /dev/null
+    # Repo already created or refreshed in get_wp_gitver()
+#    git clone https://github.com/WiringPi/WiringPi
     cd WiringPi
     ./build
     gpio -v
