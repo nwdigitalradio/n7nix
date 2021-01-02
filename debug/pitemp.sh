460db845c32472d9afd4881193c315c085d30f09
n7nix <basil@pacabunga.com>
2021-01-02 07:29:21 -0800

    Fill in 2 more bit definitions from get-throttled reg.

--- debug/pitemp.sh
+++ debug/pitemp.sh
@@ -6,8 +6,10 @@ if [[ "$throt" != 0 ]] ; then
     echo "   0: under-voltage"
     echo "   1: arm frequency capped"
     echo "   2: currently throttled"
+    echo "   3: soft temp limit active"
     echo "  16: under-voltage has occurred"
-    echo "  17: arm frequency capped has occurred"
+    echo "  17: ARM frequency capped has occurred"
     echo "  18: throttling has occurred"
+    echo "  19: soft temp limit has occurred"
 fi
 
