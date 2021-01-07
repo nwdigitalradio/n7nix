e498abcf079f0cd93dce5b0a1e3570117255f23c
n7nix <basil@pacabunga.com>
2021-01-06 23:15:13 -0800

    Add install script for chattervox

---
+++ keyb2keyb/cv_install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#
+# Install chattervox from github source repository
+#
+USER=$(whoami)
+REPO_DIR="/home/$USER/dev/github"
+PROG_NAME="chattervox"
+
+# ===== function error_exit
+# Display string argument & exit
+
+function error_exit() {
+	echo "$(tput setaf 1)$1$(tput sgr0)" 1>&2
+	exit 1
+}
+
+#
+# ===== main
+#
+# Check for repository directory
+if [ ! -e "$REPO_DIR" ] ; then
+    mkdir -p "$REPO_DIR"
+fi
+
+cd $REPO_DIR
+if [ -e "$PROG_NAME" ] ; then
+    echo
+    echo "Found an existing $REPO_DIR/$PROG_NAME, removing"
+    echo
+    sudo rm -R "$PROG_NAME"
+
+    # Check if chattervox is running
+    pidof_main=$(pgrep -f "node build/main.js")
+    if [ "$?" -eq 0 ] ; then
+        echo "Stopping running $PROG_NAME"
+	kill $pidof_main
+    fi
+fi
+
+# clone the chattervox repo
+echo "$(tput setaf 6)Clone $PROG_NAME repo$(tput sgr0)"
+git clone https://github.com/brannondorsey/chattervox
+if [ "$?" -ne 0 ] ; then
+    error_exit "Error cloning $PROG_NAME repo"
+fi
+
+cd $PROG_NAME
+
+# download dependencies
+echo "$(tput setaf 6)Download dependencies (npm install)$(tput sgr0)"
+npm install
+if [ "$?" -ne 0 ] ; then
+    error_exit "Error doing npm install"
+fi
+
+# transpile the src/*.ts typescript files to build/*.js
+echo "$(tput setaf 6)Build $PROG_NAME$(tput sgr0)"
+npm run build
+if [ "$?" -ne 0 ] ; then
+    error_exit "Error on npm run build"
+fi
+
+# Verify the key pair exists
+echo "$(tput setaf 6)Display keys$(tput sgr0)"
+node build/main.js showkey
+if [ "$?" -ne 0 ] ; then
+    error_exit "Error on $PROG_NAME showkey"
+fi
+
+# If configuration files do NOT exist then running the send command will
+# prompt for required information & generate an ECDSA keypair
+#  ECDSA - Elliptic Curve Digital Signature Algorithm
+
+echo "$(tput setaf 6)Send first test/config message$(tput sgr0)"
+
+node build/main.js send "Install test on $(date)"
+if [ "$?" -ne 0 ] ; then
+    error_exit "Error on $PROG_NAME send, init call"
+fi
+
+# Reference:
+#
+# generate a new public/private key pair, and use it as your default signing key
+# chattervox genkey --make-signing
+#
+# run chattervox from source to opening the chat room
+# node build/main.js chat
