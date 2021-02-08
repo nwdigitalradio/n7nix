501f1d759182b5492a6e2fb7e5aa338c83171ef5
n7nix <basil@pacabunga.com>
2021-02-08 12:49:09 -0800

    Handle case of Microsoft repo has yet to be installed.

--- bin/block_msvscode.sh
+++ bin/block_msvscode.sh
@@ -6,32 +6,55 @@
 # Reference:
 #  https://www.cyberciti.biz/linux-news/heads-up-microsoft-repo-secretly-installed-on-all-raspberry-pis-linux-os/
 #
+scriptname="`basename $0`"
+VERSION="1.1"
 
 FILE_MICROSOFT_KEY="/etc/apt/trusted.gpg.d/microsoft.gpg"
 FILE_MICROSOFT_APT="/etc/apt/sources.list.d/vscode.list"
+FILE_HOSTS="/etc/hosts"
 
 function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
 
+# function make a benign Microsoft VS Code apt file
+
+function make_benign_msaptfile() {
+
+    sudo tee "$FILE_MICROSOFT_APT" > /dev/null << EOF
+#### THIS FILE IS AUTOMATICALLY CONFIGURED ###
+## You may comment out this entry, but any other modifications may be lost.
+#deb [arch=amd64,arm64,armhf] http://packages.microsoft.com/repos/code stable main
+EOF
+}
+
+# function make an empty Microsoft VS Code key file
+
+function make_empty_mskeyfile() {
+
+    if [ -f "$FILE_MICROSOFT_KEY" ] ; then
+        echo "Removing Microsoft key"
+        sudo rm -vf "$FILE_MICROSOFT_KEY"
+    fi
+
+    # Make sure new keys can NOT be installed
+    echo "  Making new empty Microsoft key file"
+    sudo touch "$FILE_MICROSOFT_KEY"
+    sudo chattr +i "$FILE_MICROSOFT_KEY"
+
+}
 # function block Microsoft VS Code
 
 function block_ms() {
 
     ## hosts file
-    grep -iq "0.0.0.0 packages.microsoft.com" /etc/hosts
+    grep -iq "0.0.0.0 packages.microsoft.com" $FILE_HOSTS
     if [ $? != 0 ] ; then
-        sudo echo "0.0.0.0 packages.microsoft.com" | sudo tee -a  /etc/hosts
+        echo "0.0.0.0 packages.microsoft.com" | sudo tee -a $FILE_HOSTS
     fi
 
     ## gpg key
     # Delete Microsoft's GPG key
     if [[ -e "$FILE_MICROSOFT_KEY" ]] && [[ -s "$FILE_MICROSOFT_KEY" ]]  ; then
-        echo "Removing Microsoft key"
-        sudo rm -vf "$FILE_MICROSOFT_KEY"
-
-        # Make sure new keys can NOT be installed
-        echo "Making new empty Microsoft key file"
-        sudo touch "$FILE_MICROSOFT_KEY"
-        sudo chattr +i "$FILE_MICROSOFT_KEY"
+        make_empty_mskeyfile
     else
         echo "Microsoft key either does NOT exist OR is empty."
     fi
@@ -41,8 +64,8 @@ function block_ms() {
     lsattr "$FILE_MICROSOFT_KEY"
 
     ## package file
-   grep -iq "^deb" $FILE_MICROSOFT_APT
-   if [ $? = 0 ] ; then
+    grep -iq "^deb" $FILE_MICROSOFT_APT
+    if [ $? = 0 ] ; then
         echo "Modifying Microsoft APT file"
         # Add a comment character for all lines in VSCode apt file
         sudo sed -i -e 's/^/#/' "$FILE_MICROSOFT_APT"
@@ -58,16 +81,31 @@ function block_ms() {
 
 function is_microsoft() {
 
-   grep -iq "^deb" $FILE_MICROSOFT_APT
-   grepcode=$?
-   dbgecho "File $FILE_MICROSOFT_APT : $grepcode"
-
-   if [ $grepcode = 0 ] || ( [[ -f "$FILE_MICROSOFT_KEY" ]] && [[ -s "$FILE_MICROSOFT_KEY" ]] ) ; then
-       echo "  Microsoft gpg key or repository entry IS active"
-       retcode=0
+   grepcode=1
+   if [ -f $FILE_MICROSOFT_APT ] ; then
+       grep -iq "^deb" $FILE_MICROSOFT_APT
+       grepcode=$?
+       dbgecho "File $FILE_MICROSOFT_APT : $grepcode"
+       APT_FILE_EXISTS=true
    else
-       echo "  Microsoft gpg key and repository entry NOT active"
-       retcode=1
+       echo "  File: $FILE_MICROSOFT_APT does NOT exist"
+       APT_FILE_EXISTS=false
+   fi
+
+   retcode=1
+
+   if [ -f "$FILE_MICROSOFT_KEY" ] ; then
+       KEY_FILE_EXISTS=true
+
+       if [ $grepcode = 0 ] || [[ -s "$FILE_MICROSOFT_KEY" ]] ; then
+           echo "  Microsoft gpg key or repository entry IS active"
+           retcode=0
+       else
+           echo "  Microsoft gpg key and repository entry NOT active"
+       fi
+   else
+       echo "  File: $FILE_MICROSOFT_KEY does NOT exist"
+       KEY_FILE_EXISTS=false
    fi
 
    return $retcode
@@ -78,7 +116,7 @@ function is_microsoft() {
 function block_ms_status() {
 
 ## Status of hosts file
-FILE_HOSTS="/etc/hosts"
+
 grep -iq "0.0.0.0 packages.microsoft.com" $FILE_HOSTS
 if [ $? != 0 ] ; then
     echo "  File: $FILE_HOSTS has NOT been edited"
@@ -111,7 +149,7 @@ fi
 # If there are any command line parameters just display status of of
 # Microsoft files
 
-echo "Test if Microsoft repo has been configured"
+echo "${scriptname} ver: ${VERSION}: Test if Microsoft repo has been configured"
 
 # Check for any command line arguments
 if [[ $# -gt 0 ]] ; then
@@ -119,9 +157,26 @@ if [[ $# -gt 0 ]] ; then
     exit 0
 fi
 
+# Check if Microsoft repo file already exists
 if is_microsoft ; then
     dbgecho "  Will call block_ms"
     block_ms
 else
-    echo "  No files edited Microsoft repository already blocked."
+    if [[ $KEY_FILE_EXISTS = true ]] && [[ $KEY_FILE_EXISTS = true ]] ; then
+        echo "  No files edited Microsoft repository already blocked."
+    else
+        # Case where msvscode has not been added to repository yet.
+        ## hosts file
+        grep -iq "0.0.0.0 packages.microsoft.com" $FILE_HOSTS
+        if [ $? != 0 ] ; then
+            echo "0.0.0.0 packages.microsoft.com" | sudo tee -a $FILE_HOSTS
+        fi
+
+        ## gpg key
+        make_empty_mskeyfile
+
+	## package file
+	make_benign_msaptfile
+        echo "  1 file edited, 2 files created Microsoft repository now blocked."
+    fi
 fi
