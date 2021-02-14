aa9d7aa1ecd9be276de9049e4d3e0442b9d1bd9f
n7nix <basil@pacabunga.com>
2021-02-14 11:17:39 -0800

    Display information to characterize kern ver 5.10.11 bug

---
+++ bin/buginfo.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Display information to debug DRAWS codec not enumerated
+#
+# Problem first found with kernel:
+#  5.10.11-v7l+ #1399 SMP Thu Jan 28 12:09:48 GMT 2021
+# on Feb. 13, 2021
+
+
+# ===== function check udrc enumeration
+function check_udrc() {
+    CARDNO=$(aplay -l | grep -i udrc)
+
+    if [ ! -z "$CARDNO" ] ; then
+        echo "udrc card number line: $CARDNO"
+        CARDNO=$(echo $CARDNO | cut -d ' ' -f2 | cut -d':' -f1)
+        echo "udrc is sound card #$CARDNO"
+    else
+        echo " No udrc sound card found"
+    fi
+}
+
+# ===== main
+
+echo "== Kernel version:"
+uname -a
+
+echo
+echo "== Codec driver check:"
+dmesg | grep -i "tlv320a"
+
+echo
+echo "== DRAWS driver check:"
+check_udrc
+
+echo
+echo "== Pi Version"
+piver.sh
+
+echo
+echo "== /boot/config"
+tail /boot/config.txt
