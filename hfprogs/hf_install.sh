0642bee9081cef71c2ddb56f3ff3939fb7897d30
n7nix <basil@pacabunga.com>
2021-02-15 13:34:49 -0800

    Bump hamlib version number.

--- hfprogs/hf_install.sh
+++ hfprogs/hf_install.sh
@@ -346,7 +346,7 @@ if [[ $# -eq 1 ]] && [[ "$1" -eq "$USER" ]] ; then
     # Build js8call first to satisfy some wsjtx dependencies
     build_js8call "2.1.1"
     build_wsjtx "2.1.2"
-    build_hamlib "3.3"
+    build_hamlib "4.1"
     build_flapp "0.1.4" flxmlrpc
     # Must build fldigi before the other apps
     # apps rely on /usr/bin/fltk-config
