6a2276202b4d13e9cc42a7c915b16cb55621afab
n7nix <basil@pacabunga.com>
2021-03-02 11:42:17 -0800

    Add start service if any command line args, allow running as user.

--- bin/mgr-stop.sh
+++ bin/mgr-stop.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 #
-# Script to stop WiFi access point
-# The script disables & stops the services
+# Script to stop DRAWS manager systemd service
+# The script disables & stops the service
+
+
+USER=
+SYSTEMCTL="systemctl"
 
 SERVICE_LIST="draws-manager"
+
+# ===== function start_service
+function start_service() {
+    service="$1"
+    echo "Starting: $service"
+
+    systemctl is-enabled "$service" > /dev/null 2>&1
+    if [ $? -ne 0 ] ; then
+        echo "ENABLING $service"
+        $SYSTEMCTL enable "$service"
+        if [ "$?" -ne 0 ] ; then
+            echo "Problem ENABLING $service"
+        fi
+    fi
+    $SYSTEMCTL --no-pager start "$service"
+    if [ "$?" -ne 0 ] ; then
+        echo "Problem starting $service"
+    fi
+}
 
 # ===== function stop_service
 
@@ -12,16 +35,16 @@ function stop_service() {
     systemctl is-enabled "$service" > /dev/null 2>&1
     if [ $? -eq 0 ] ; then
         echo "DISABLING $service"
-        systemctl disable "$service"
+        $SYSTEMCTL disable "$service"
         if [ "$?" -ne 0 ] ; then
             echo "Problem DISABLING $service"
         fi
     else
         echo "Service: $service already disabled."
     fi
-    systemctl stop "$service"
+    $SYSTEMCTL stop "$service"
     if [ "$?" -ne 0 ] ; then
-        echo "Problem stopping $service"
+        echo "Problem STOPPING $service"
     fi
 }
 
@@ -29,11 +52,18 @@ function stop_service() {
 
 # Be sure we're running as root
 if [[ $EUID != 0 ]] ; then
-   echo "Must be root"
-   exit 1
+    SYSTEMCTL="sudo systemctl"
+    USER=$(whoami)
+    dbgecho "set sudo as user $USER"
 fi
 
-for service in `echo ${SERVICE_LIST}` ; do
-#    echo "DEBUG: Stopping service: $service"
-    stop_service $service
-done
+# default to stop/disable draws-manager
+if [[ $# -eq 0 ]] ; then
+    for service in `echo ${SERVICE_LIST}` ; do
+        # echo "DEBUG: Stopping service: $service"
+        stop_service $service
+    done
+else
+    # Any command line arguments then start draws-manager service
+    start_service draws-manager
+fi
