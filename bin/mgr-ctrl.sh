49f2ff7e4fa38c27a0d584b9bb8584b0ab53a0e3
n7nix <basil@pacabunga.com>
2021-03-08 10:30:43 -0800

    Add start, stop, status functions.

--- bin/mgr-ctrl.sh
+++ bin/mgr-ctrl.sh
@@ -3,11 +3,29 @@
 # Script to stop DRAWS manager systemd service
 # The script disables & stops the service
 
-
+DEBUG=
 USER=
 SYSTEMCTL="systemctl"
-
 SERVICE_LIST="draws-manager"
+
+function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
+
+# ===== function status_service
+
+function status_service() {
+    service="$1"
+    IS_ENABLED="ENABLED"
+    IS_RUNNING="RUNNING"
+    # echo "Checking service: $service"
+    systemctl is-enabled "$service" > /dev/null 2>&1
+    if [ $? -ne 0 ] ; then
+        IS_ENABLED="NOT ENABLED"
+    fi
+    systemctl is-active "$service" > /dev/null 2>&1
+    if [ $? -ne 0 ] ; then
+        IS_RUNNING="NOT RUNNING"
+    fi
+}
 
 # ===== function start_service
 function start_service() {
@@ -48,22 +66,49 @@ function stop_service() {
     fi
 }
 
+function mgr_stop() {
+
+    for service in `echo ${SERVICE_LIST}` ; do
+        dbgecho "DEBUG: Stopping service: $service"
+        stop_service $service
+    done
+}
+
+function mgr_status() {
+    for service in `echo ${SERVICE_LIST}` ; do
+        status_service $service
+        echo "Status for $service: $IS_RUNNING and $IS_ENABLED"
+    done
+}
+
 # ===== main
 
-# Be sure we're running as root
+# Check if running as root
 if [[ $EUID != 0 ]] ; then
     SYSTEMCTL="sudo systemctl"
     USER=$(whoami)
     dbgecho "set sudo as user $USER"
 fi
 
-# default to stop/disable draws-manager
-if [[ $# -eq 0 ]] ; then
-    for service in `echo ${SERVICE_LIST}` ; do
-        # echo "DEBUG: Stopping service: $service"
-        stop_service $service
-    done
-else
-    # Any command line arguments then start draws-manager service
-    start_service draws-manager
-fi
+while [[ $# -gt 0 ]] ; do
+APP_ARG="$1"
+
+case $APP_ARG in
+    stop)
+        mgr_stop
+        exit 0
+    ;;
+    start)
+        start_service draws-manager
+	exit 0
+    ;;
+    status)
+        mgr_status
+	exit 0
+    ;;
+esac
+
+shift # past argument
+done
+
+mgr_status
