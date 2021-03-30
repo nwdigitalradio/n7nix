f6b6cb54021f52fa39d6440f1b45e1d931688812
n7nix <basil@pacabunga.com>
2021-03-30 09:00:08 -0700

    Fix direwolf version display for non dev case.

--- direwolf/dw_ver.sh
+++ direwolf/dw_ver.sh
@@ -2,11 +2,11 @@
 #
 #dire_ver=$(direwolf -v 2>/dev/null | grep -m 1 -i version | cut -d " " -f4)
 dire_ver=$(direwolf -v 2>/dev/null | grep -m 1 -i version)
-grep -i development <<< $dire_ver >/dev/null 2>&1
-if [ "$?" ] ; then
+grep -i "development" <<< $dire_ver >/dev/null 2>&1
+if [ "$?" -eq 0 ] ; then
     dire_verx=$(echo $dire_ver | cut -d " " -f5)
 else
-    dire_vexr=$(echo $dire_ver | cut -d " " -f4)
+    dire_verx=$(echo $dire_ver | cut -d " " -f4)
 fi
 
 echo "Found direwolf version: ${dire_verx#*D} : D${dire_ver#*D}"
