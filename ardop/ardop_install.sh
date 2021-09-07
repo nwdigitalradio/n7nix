53882821bceaec18672ee1611df68ec85b1faed9
n7nix <basil@pacabunga.com>
2021-09-07 08:44:03 -0700

    update arim & garim version numbers for install.

--- ardop/ardop_install.sh
+++ ardop/ardop_install.sh
@@ -221,10 +221,10 @@ if [ "$needs_pkg" = "true" ] ; then
    fi
 fi
 
-# garim 1.2
-# wget https://www.whitemesa.net/garim/src/garim-1.2.tar.gz
+# garim 1.5
+# wget https://www.whitemesa.net/garim/src/garim-1.5.tar.gz
 
-arim_ver="2.10"
+arim_ver="2.11"
 download_filename="arim-${arim_ver}.tar.gz"
 ARIM_SRC_DIR=$SRC_DIR/arim-$arim_ver
 
