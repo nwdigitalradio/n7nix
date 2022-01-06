977ba0f6fa0e255d2331da203a3e241b6d4743fb
n7nix <basil@pacabunga.com>
2022-01-06 12:48:05 -0800

    Take out check for running as root

--- change_user/change_host.sh
+++ change_user/change_host.sh
@@ -4,28 +4,72 @@
 # - /etc/hostname
 # - /etc/mailname
 # - /etc/hosts
+DEBUG=
 
 scriptname="`basename $0`"
 
+function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
+
+# ===== function get_hostname
+# Validate hostname
+#   https://stackoverflow.com/questions/20763980/check-if-a-string-contains-only-specified-characters-including-underscores/20764037
+
+function get_hostname() {
+
+    #  Clear the read buffer
+    read -t 1 -n 10000 discard
+
+    read -ep "Enter new host name followed by [enter]: " HOSTNAME
+
+    # From hostname(7) man page
+    # Valid characters for hostnames are ASCII(7) letters from a to z,
+    # the digits from 0 to 9, and the hyphen (-).
+    # A hostname may not start with a hyphen.
+    if [[ $HOSTNAME =~ ^[a-z0-9\-]+$ ]]; then
+        dbgecho "str: $HOSTNAME  matches"
+        return 0
+    else
+        dbgecho "str: $HOSTNAME does NOT match"
+        return 1
+    fi
+}
+
+# ===== main
+
 # Be sure we're running as root
-if [[ $EUID != 0 ]] ; then
-   echo "Must be root"
-   exit 1
-fi
+#if [[ $EUID != 0 ]] ; then
+#   echo "Must be root"
+#   exit 1
+#fi
+
+hostname_default="draws"
+HOSTNAME=$(cat /etc/hostname | tail -1)
 
 # Check hostname
-echo " === Verify hostname"
-HOSTNAME=$(cat /etc/hostname | tail -1)
-echo "$scriptname: Current hostname: $HOSTNAME"
+echo " === Current hostname: $HOSTNAME"
 
-   # Change hostname
-   echo "Enter new host name followed by [enter]:"
-   read -t 1 -n 10000 discard
-   read -e HOSTNAME
-   echo "$HOSTNAME" > /etc/hostname
+# Change hostname
+
+while  ! get_hostname ; do
+    echo "Input error for $HOSTNAME, try again"
+    echo "Valid characters for hostnames are:"
+    echo " letters from a to z,"
+    echo " the digits from 0 to 9,"
+    echo " and the hyphen (-)"
+done
+
+if [ ! -z "$HOSTNAME" ] ; then
+    echo "Setting new hostname: $HOSTNAME"
+else
+    echo "Setting hostname to default: $hostname_default"
+    HOSTNAME="$hostname_default"
+fi
+
+#echo "$HOSTNAME" > /etc/hostname
+echo "$HOSTNAME" | sudo tee /etc/hostname > /dev/null
 
 echo "=== Set mail hostname"
-echo "$HOSTNAME.localhost" > /etc/mailname
+echo "$HOSTNAME.localhost" | sudo tee /etc/mailname > /dev/null
 
 # Be sure system host name can be resolved
 
@@ -36,13 +80,13 @@ if [ $? -eq 0 ] ; then
    HOSTNAME_CHECK=$(grep "127.0.1.1" /etc/hosts | awk {'print $2'})
    if [ "$HOSTNAME" != "$HOSTNAME_CHECK" ] ; then
       echo "Make host names match between /etc/hostname & /etc/hosts"
-      sed -i -e "/127.0.1.1/ s/127.0.1.1\t.*/127.0.1.1\t$HOSTNAME ${HOSTNAME}.localnet/" /etc/hosts
+      sudo sed -i -e "/127.0.1.1/ s/127.0.1.1\t.*/127.0.1.1\t$HOSTNAME ${HOSTNAME}.localnet/" /etc/hosts
    else
       echo "host names match between /etc/hostname & /etc/hosts"
    fi
 else
    # Add a 127.0.1.1 entry to /etc/hosts
-   sed -i '1i\'"127.0.1.1\t$HOSTNAME $HOSTNAME.localnet" /etc/hosts
+   sudo sed -i '1i\'"127.0.1.1\t$HOSTNAME $HOSTNAME.localnet" /etc/hosts
    if [ $? -ne 0 ] ; then
       echo "Failed to modify /etc/hosts file"
    fi
@@ -51,4 +95,4 @@ fi
 # Get hostname to verify
 #HOSTNAME=$(cat /etc/hostname | tail -1)
 
-echo "FINISHED changing host names"
+echo "FINISHED changing host name to: $HOSTNAME"
