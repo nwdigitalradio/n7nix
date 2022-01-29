e3e7e9a294d1e41d2c4d817dfd4dc8f9ce6bf95d
n7nix <basil@pacabunga.com>
2022-01-29 10:58:17 -0800

    Make hamlib check a function check_hamlib()

--- bin/buginfo.sh
+++ bin/buginfo.sh
@@ -51,6 +51,56 @@ function draws_ver() {
     fi
 }
 
+# function check_hamlib
+# Verify that there is only a single hamlib directory
+
+function check_hamlib() {
+    echo
+    echo "== hamlib check"
+
+    arm_hamlib_cnt=0
+    local_hamlib_cnt=0
+
+    # Check for older versions of hamlib
+    hamlib_dir="/usr/lib/arm-linux-gnueabihf"
+    if [ -d "$hamlib_dir" ] && [ -e $hamlib_dir/libhamlib.so.4 ] ; then
+        libcnt=$(ls -1 $hamlib_dir/libhamlib* | wc -l)
+        if ((libcnt > 0 )) ; then
+            arm_hamlib_cnt=$libcnt
+            echo "hamlib: Found $arm_hamlib_cnt hamlib files in $hamlib_dir"
+            ls -alt $hamlib_dir/libhamlib*
+	    echo
+        else
+            echo "hamlib: NO hamlib files found in $hamlib_dir"
+        fi
+    else
+        echo "hamlib directory: $hamlib_dir files do NOT exist"
+    fi
+
+    # Check for newer versions of hamlib
+    hamlib_dir="/usr/local/lib"
+    if [ -d "$hamlib_dir" ] && [ -e "$hamlib_dir/libhamlib.so.4" ] ; then
+        libcnt=$(ls -1 $hamlib_dir/libhamlib* | wc -l)
+        if ((libcnt > 0 )) ; then
+            local_hamlib_cnt=$libcnt
+            echo "hamlib: Found $local_hamlib_cnt hamlib files in $hamlib_dir"
+            ls -alt $hamlib_dir/libhamlib*
+        else
+            echo "hamlib: NO hamlib files found in $hamlib_dir"
+        fi
+    else
+        echo "hamlib directory: $hamlib_dir files do NOT exist"
+    fi
+
+    echo
+    hamlib_dir="/usr/lib/arm-linux-gnueabihf"
+    if ((arm_hamlib_cnt > 0)) && ((local_hamlib_cnt > 0)) ; then
+        echo "$(tput setaf 6)Need to remove conflicting hamlib files from: $hamlib_dir$(tput sgr0)"
+    else
+        echo "No conflicting hamlib files found"
+    fi
+}
+
 # ===== main
 
 echo "=== Versions ==="
@@ -101,36 +151,7 @@ if [ $? -ne 0 ] ; then
     echo "gpsd OK"
 fi
 
-echo
-echo "== hamlib check"
-
-# Check for older versions of hamlib
-hamlib_dir="/usr/lib/arm-linux-gnueabihf"
-if [ -d "$hamlib_dir" ] && [ -e $hamlib_dir/libhamlib.so.4 ] ; then
-    libcnt=$(ls -1 $hamlib_dir/libhamlib* | wc -l)
-    if ((libcnt > 0 )) ; then
-        echo "hamlib: Found $libcnt hamlib files in $hamlib_dir"
-        ls -alt $hamlib_dir/libhamlib*
-    else
-        echo "hamlib: NO hamlib files found in $hamlib_dir"
-    fi
-else
-    echo "hamlib directory: $hamlib_dir files do NOT exist"
-fi
-
-# Check for newer versions of hamlib
-hamlib_dir="/usr/local/lib"
-if [ -d "$hamlib_dir" ] && [ -e "$hamlib_dir/libhamlib.so.4" ] ; then
-    libcnt=$(ls -1 $hamlib_dir/libhamlib* | wc -l)
-    if ((libcnt > 0 )) ; then
-        echo "hamlib: Found $libcnt hamlib files in $hamlib_dir"
-        ls -alt $hamlib_dir/libhamlib*
-    else
-        echo "hamlib: NO hamlib files found in $hamlib_dir"
-    fi
-else
-    echo "hamlib directory: $hamlib_dir files do NOT exist"
-fi
+check_hamlib
 
 echo
 echo "== /boot/config file"
