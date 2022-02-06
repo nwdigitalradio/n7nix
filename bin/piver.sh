0e1225e75711a81b9b28ad8771e52be496503ec4
n7nix <basil@pacabunga.com>
2022-02-06 13:36:29 -0800

    Fix display of CPU clock speed

--- bin/piver.sh
+++ bin/piver.sh
@@ -31,79 +31,79 @@ piver="$(echo -e "${piver##*:}" | tr -d '[[:space:]]')"
 
 case $piver in
 9020e0)
-   VERSION_STRING=" Pi 3 Model A+, Rev 1.0, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 3 Model A+, Rev 1.0, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 a01040)
-   VERSION_STRING=" Pi 2 Model B, Rev 1.0, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 2 Model B, Rev 1.0, Mfg by Sony UK"
 ;;
 a01041)
-   VERSION_STRING=" Pi 2 Model B, Rev 1.1, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 2 Model B, Rev 1.1, Mfg by Sony UK"
 ;;
 a02082)
-   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 a020d3)
-   VERSION_STRING=" Pi 3 Model B+, Rev 1.3, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 3 Model B+, Rev 1.3, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 a21041)
-   VERSION_STRING=" Pi 2 Model B, Rev 1.1, Mfg by Embest, $cpu_clk MHz"
+   VERSION_STRING=" Pi 2 Model B, Rev 1.1, Mfg by Embest"
 ;;
 a22042)
-   VERSION_STRING=" Pi 2 Model B with BCM2837, Rev 1.2, Mfg by Embest, $cpu_clk MHz"
+   VERSION_STRING=" Pi 2 Model B with BCM2837, Rev 1.2, Mfg by Embest"
 ;;
 a22082)
-   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Embest, $cpu_clk MHz"
+   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Embest"
    HAS_WIFI=true
 ;;
 a32082)
-   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Sony Japan, $cpu_clk MHz"
+   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Sony Japan"
    HAS_WIFI=true
 ;;
 a52082)
-   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Stadium, $cpu_clk MHz"
+   VERSION_STRING=" Pi 3 Model B, Rev 1.2, Mfg by Stadium"
    HAS_WIFI=true
 ;;
 a22083)
-   VERSION_STRING=" Pi 3 Model B, Rev 1.3, Mfg by Embest, $cpu_clk MHz"
+   VERSION_STRING=" Pi 3 Model B, Rev 1.3, Mfg by Embest"
    HAS_WIFI=true
 ;;
 a03111)
-   VERSION_STRING=" Pi 4 Model B, Rev 1.1, 1GB mem, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.1, 1GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 b03111)
-   VERSION_STRING=" Pi 4 Model B, Rev 1.1, 2GB mem, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.1, 2GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 b03112)
-   VERSION_STRING=" Pi 4 Model B, Rev 1.2, 2GB mem, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.2, 2GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 b03114)
-   VERSION_STRING=" Pi 4 Model B, Rev 1.4, 2GB mem, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.4, 2GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 c03111)
-   VERSION_STRING=" Pi 4 Model B, Rev 1.1, 4GB mem, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.1, 4GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 c03112)
-   VERSION_STRING=" Pi 4 Model B, Rev 1.2, 4GB mem, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.2, 4GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 c03114)
-   VERSION_STRING=" Pi 4 Model B, Rev 1.4, 4GB mem, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.4, 4GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 d03114)
-   VERSION_STRING=" Pi 4 Model B, Rev 1.4, 8GB mem, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 4 Model B, Rev 1.4, 8GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 c03130)
-   VERSION_STRING=" Pi 400, Rev 1.0, 4GB mem, Mfg by Sony UK, $cpu_clk MHz"
+   VERSION_STRING=" Pi 400, Rev 1.0, 4GB mem, Mfg by Sony UK"
    HAS_WIFI=true
 ;;
 *)
@@ -125,6 +125,6 @@ else
         retcode=0
     fi
 
-    echo "$VERSION_STRING $WIFI_STRING"
+    echo "$VERSION_STRING, $cpu_clk MHz $WIFI_STRING"
 fi
 exit $retcode
