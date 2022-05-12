de139e2f4a87f65a482c3b43ad35b8e545885806
n7nix <basil@pacabunga.com>
2022-05-12 09:55:13 -0700

    Add detect of running from an ssh session, which will not work.

--- bin/which-desktop.sh
+++ bin/which-desktop.sh
@@ -3,4 +3,8 @@
 # which-desktop.sh
 #
 
+if [ -z "$XDG_CURRENT_DESKTOP" ] && [ -z "$GDMSESSION" ] ; then
+    echo "Try running this script from a console launched from the window manager"
+    exit 0
+fi
 printf 'Desktop: %s\nSession: %s\n' "$XDG_CURRENT_DESKTOP" "$GDMSESSION"
