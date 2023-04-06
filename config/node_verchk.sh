94e5f447f5f59a726a9b4d1ae5419a38064ef2b8
n7nix <basil@pacabunga.com>
2023-04-06 14:27:25 -0700

    Make a routine to be added to scripts trying to install npm

--- config/node_verchk.sh
+++ config/node_verchk.sh
@@ -2,41 +2,59 @@
 #
 # Check for current version of node, npm, nvm
 
-source ~/.nvm/nvm.sh
+# ===== function dbgecho
+
+function dbgecho { if [ ! -z "$DEBUG" ] ; then echo "$*"; fi }
+
+# ===== function node_ver
+function node_ver() {
+
+    source ~/.nvm/nvm.sh
 
 # expect
-#  v19.8.1
-#  0
-#  9.5.1
-#  0
-#  0.39.3
-#  0
+  #  v19.8.1
+  #  0
+  #  9.5.1
+  #  0
+  #  0.39.3
+  #  0
 
 if [ ! -z "$DEBUG" ] ; then
     echo "debug:"
     node --version ; echo $? ; npm --version ; echo $? ; nvm --version ; echo $?
 fi
 
-echo "==== Installed versions ===="
-node_ver=$(node --version)
-node_ret=$?
-# Remove leading 'V'
-node_ver="${node_ver:1}"
-if [ $node_ret -eq 0 ] ; then
-    echo "node: $node_ver"
-fi
+    dbgecho "==== Installed versions ===="
+    # Display node version
+    node_ver=$(node --version)
+    node_ret=$?
+    if [ $node_ret -eq 0 ] ; then
+        # Remove leading 'V'
+        node_ver="${node_ver:1}"
+        echo "node: $node_ver"
+    else
+        echo "node NOT installed"
+    fi
 
-npm_ver=$(npm --version)
-npm_ret=$?
-if [ $npm_ret -eq 0 ] ; then
-    echo "npm: $npm_ver"
-fi
+    # Display npm (Node Package Manager) version
+    npm_ver=$(npm --version)
+    npm_ret=$?
+    if [ $npm_ret -eq 0 ] ; then
+        echo "npm: $npm_ver"
+    else
+        echo "npm NOT installed"
+    fi
 
-nvm_ver=$(nvm --version)
-nvm_ret=$?
-if [ $nvm_ret -eq 0 ] ; then
-    echo "nvm: $nvm_ver"
-else
-    echo "nvm NOT installed"
-fi
+    # Display nvm (Node Version Manager) version
+    nvm_ver=$(nvm --version)
+    nvm_ret=$?
+    if [ $nvm_ret -eq 0 ] ; then
+        echo "nvm: $nvm_ver"
+    else
+        echo "nvm NOT installed"
+    fi
+}
 
+# ===== main
+
+node_ver
